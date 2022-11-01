7aa63ab4990d2d608b6f1fb9e212555931d42a96
Jon Suderman <jon@suderman.net>
2022-11-01 00:27:54 -0600

    conf

--- hosts/nimbus/configuration.nix
+++ hosts/nimbus/configuration.nix
@@ -143,6 +143,8 @@ in {
 
   virtualisation.docker.enable = true;
 
+  programs.nix-ld.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
