b60508ceed64429530524dd14a9f4081035cfcb8
Jon Suderman <jon@suderman.net>
2022-11-08 14:56:28 -0700

    fix keyd

--- nixos/keyd.nix
+++ nixos/keyd.nix
@@ -1,8 +1,13 @@
 { pkgs, ... }:
 {
-  users.groups.keyd.name = "keyd";
+
+  # Install keyd package
   environment.systemPackages = [ pkgs.keyd ];
 
+  # Create keyd group
+  users.groups.keyd.name = "keyd";
+
+  # Create systemd service
   systemd.services.keyd = {
     description = "key remapping daemon";
     requires = [ "local-fs.target" ];
@@ -10,229 +15,172 @@
     wantedBy = [ "sysinit.target" ];
     serviceConfig = {
       Type = "simple";
-      ExecStart = "${keyd}/bin/keyd";
+      ExecStart = "${pkgs.keyd}/bin/keyd";
       # Restart = "on-failure";
     };
   };
 
-  # 644 /usr/share/libinput/30-keyd.quirks
-  # /etc/libinput/local-overrides.quirks
-  environment.etc."libinput/local-overrides.quirks" = {
-    mode = "0644";
-    text = ''
-      [keyd]
-      MatchUdevType=keyboard
-      MatchVendor=0xFAC
-      AttrKeyboardIntegration=internal
-    '';
-  };
+  # Add quirks to make touchpad's "disable-while-typing" work properly
+  environment.etc."libinput/local-overrides.quirks".text = ''
+    [keyd]
+    MatchUdevType=keyboard
+    MatchVendor=0xFAC
+    AttrKeyboardIntegration=internal
+  '';
 
-  environment.etc = {
-    "keyd/default.conf".source = pkgs.writeText "keyd.conf" ''
+  # Configuration for keyd
+  environment.etc."keyd/default.conf".text = ''
+    [ids]
 
-      [ids]
+    # Framework Laptop
+    # AT Translated Set 2 keyboard
+    0001:0001
 
-      # Framework Laptop
-      # AT Translated Set 2 keyboard
-      0001:0001
+    # Tapping both shift keys will activate capslock.
+    [shift]
 
-      # Tapping both shift keys will activate capslock.
-      [shift]
-
-      leftshift = capslock
-      rightshift = capslock
+    leftshift = capslock
+    rightshift = capslock
 
 
-      # While holding down command/super-tab
-      [app:M]
+    # While holding down command/super-tab
+    [app:M]
 
-      # Meta-Tab: Switch to next application
-      tab = M-tab
-      right = M-tab
+    # Meta-Tab: Switch to next application
+    tab = M-tab
+    right = M-tab
 
-      # Meta-Backtick: Switch to previous application
-      ` = M-S-tab
-      left = M-S-tab
-
-
-      # Vim-like mode for navigation
-      [nav]
-
-      # vi keys
-      k = up
-      l = right
-      j = down
-      h = left
-      u = pageup
-      d = pagedown
-      w = C-right
-      b = C-left
-
-      # emacs keys
-      f = right
-      a = home
-      e = end
-      p = pageup
-      n = pagedown
-
-      # hhkb keys
-      [ = up
-      ' = right
-      / = down
-      ; = left
-
-      # Cut/Copy/Paste clipboard
-      x = S-delete
-      c = C-insert
-      v = S-insert
-
-      # escape
-      . = esc
-
-      # New command layer, inherit from meta
-      [meta_cmd:M]
-
-      # tap space to switch to nav
-      space = swap(nav)
-
-      # Open app switcher (command tab)
-      tab = swap2(app, M-tab)
-
-      # backtick: Switch to next window in the application group
-      ` = A-f6
-
-      # escape
-      . = esc
-
-      # Cut/Copy/Paste clipboard
-      x = S-delete
-      c = C-insert
-      v = S-insert
-
-      # Select tabs in many programs
-      1 = A-1
-      2 = A-2
-      3 = A-3
-      4 = A-4
-      5 = A-5
-      6 = A-6
-      7 = A-7
-      8 = A-8
-      9 = A-9
+    # Meta-Backtick: Switch to previous application
+    ` = M-S-tab
+    left = M-S-tab
 
 
-      # New command layer, inherit from ctrl
-      [control_cmd:C]
+    # Vim-like mode for navigation
+    [nav]
 
-      # tap space to switch to nav
-      space = swap(nav)
+    # vi keys
+    k = up
+    l = right
+    j = down
+    h = left
+    u = pageup
+    d = pagedown
+    w = C-right
+    b = C-left
 
-      # Open app switcher (command tab)
-      tab = swap2(app, M-tab)
+    # emacs keys
+    f = right
+    a = home
+    e = end
+    p = pageup
+    n = pagedown
 
-      # backtick: Switch to next window in the application group
-      ` = A-f6
+    # hhkb keys
+    [ = up
+    ' = right
+    / = down
+    ; = left
 
-      # escape
-      . = esc
+    # Cut/Copy/Paste clipboard
+    x = S-delete
+    c = C-insert
+    v = S-insert
 
-      # Cut/Copy/Paste clipboard
-      x = S-delete
-      c = C-insert
-      v = S-insert
+    # escape
+    . = esc
 
-      # Select tabs in many programs
-      1 = A-1
-      2 = A-2
-      3 = A-3
-      4 = A-4
-      5 = A-5
-      6 = A-6
-      7 = A-7
-      8 = A-8
-      9 = A-9
+    # New command layer, inherit from meta
+    [meta_cmd:M]
+
+    # tap space to switch to nav
+    space = swap(nav)
+
+    # Open app switcher (command tab)
+    tab = swap2(app, M-tab)
+
+    # backtick: Switch to next window in the application group
+    ` = A-f6
+
+    # escape
+    . = esc
+
+    # Cut/Copy/Paste clipboard
+    x = S-delete
+    c = C-insert
+    v = S-insert
+
+    # Select tabs in many programs
+    1 = A-1
+    2 = A-2
+    3 = A-3
+    4 = A-4
+    5 = A-5
+    6 = A-6
+    7 = A-7
+    8 = A-8
+    9 = A-9
 
 
-      # Default mapping
-      [main]
+    # New command layer, inherit from ctrl
+    [control_cmd:C]
 
-      ## before:  
-      # [Capslock]
-      # [Control] [fn] [Meta] [Alt] [Space] [Alt] [Control]
-      ## after:  
-      # [Control/Esc]
-      # [Meta] [fn] [Alt] [Control/Meta] [Space] [Meta] [Alt]
-      capslock = overload(control, esc)
-      leftcontrol = layer(meta)
-      leftmeta = layer(alt)
-      leftalt = overload(control_cmd, macro(leftmeta))
-      rightalt = layer(meta)
-      f12 = toggle(main_meta)
+    # tap space to switch to nav
+    space = swap(nav)
+
+    # Open app switcher (command tab)
+    tab = swap2(app, M-tab)
+
+    # backtick: Switch to next window in the application group
+    ` = A-f6
+
+    # escape
+    . = esc
+
+    # Cut/Copy/Paste clipboard
+    x = S-delete
+    c = C-insert
+    v = S-insert
+
+    # Select tabs in many programs
+    1 = A-1
+    2 = A-2
+    3 = A-3
+    4 = A-4
+    5 = A-5
+    6 = A-6
+    7 = A-7
+    8 = A-8
+    9 = A-9
 
 
-      # Alternative to default mapping
-      [main_meta]
+    # Default mapping
+    [main]
 
-      ## before:  
-      # [Capslock]
-      # [Control] [fn] [Meta] [Alt] [Space] [Alt] [Control]
-      ## after: 
-      # [Control/Esc]
-      # [Meta] [fn] [Alt] [Meta] [Space] [Meta] [Alt]
-      leftalt = layer(meta_cmd)
-      f12 = toggle(main)
-    '';
-  };
+    ## before:  
+    # [Capslock]
+    # [Control] [fn] [Meta] [Alt] [Space] [Alt] [Control]
+    ## after:  
+    # [Control/Esc]
+    # [Meta] [fn] [Alt] [Control/Meta] [Space] [Meta] [Alt]
+    capslock = overload(control, esc)
+    leftcontrol = layer(meta)
+    leftmeta = layer(alt)
+    leftalt = overload(control_cmd, macro(leftmeta))
+    rightalt = layer(meta)
+    f12 = toggle(main_meta)
+
+
+    # Alternative to default mapping
+    [main_meta]
+
+    ## before:  
+    # [Capslock]
+    # [Control] [fn] [Meta] [Alt] [Space] [Alt] [Control]
+    ## after: 
+    # [Control/Esc]
+    # [Meta] [fn] [Alt] [Meta] [Space] [Meta] [Alt]
+    leftalt = layer(meta_cmd)
+    f12 = toggle(main)
+  '';
 
 }
-
-# [keyd]
-# MatchUdevType=keyboard
-# MatchVendor=0xFAC
-# AttrKeyboardIntegration=internal
-
-
-
-
-# { config, pkgs, lib, ... }:
-# let cfg = config.own.keyd; in
-# with lib; with types;
-# {
-#   options.own.keyd = {
-#     enable = mkEnableOption "";
-#   };
-#
-#   config = mkIf cfg.enable {
-#     environment.etc = {
-#       "keyd/default.conf".source = pkgs.writeText "keyd.conf" ''
-#         [ids]
-#         *
-#         [main]
-#         capslock = overload(capslock, capslock)
-#         leftcontrol = layer(control)
-#         [capslock]
-#         h = left
-#         j = down
-#         k = up
-#         l = right
-#         y = home
-#         u = pageup
-#         i = pagedown
-#         o = end
-#         [control:C]
-#         h = backspace
-#         [ = esc
-#       '';
-#     };
-#
-#     systemd.services.keyd = {
-#       wantedBy = [ "graphical.target" ];
-#       serviceConfig = {
-#         Type = "simple";
-#         ExecStart = ''${pkgs.keyd}/bin/keyd'';
-#         Restart = "always";
-#       };
-#     };
-#   };
-#
-# }
