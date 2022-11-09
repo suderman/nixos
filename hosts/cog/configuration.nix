78e57b53b501649c543f6d195ecd5b96f3b121e9
Jon Suderman <jon@suderman.net>
2022-11-08 20:32:25 -0700

    save

--- hosts/cog/configuration.nix
+++ hosts/cog/configuration.nix
@@ -31,7 +31,6 @@
   # Enable the GNOME Desktop Environment.
   # services.xserver.displayManager.gdm.enable = true;
   # services.xserver.desktopManager.gnome.enable = true;
-  services.xserver.libinput.touchpad.disableWhileTyping = true;
 
   environment.systemPackages = with pkgs; [];
 
