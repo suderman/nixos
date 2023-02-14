967c0a70ed233a221246064239b8951837b7e5d6
Jon Suderman <jon@suderman.net>
2023-02-14 13:45:36 -0700

    sol

--- hosts/sol/configuration.nix
+++ hosts/sol/configuration.nix
@@ -1,40 +1,25 @@
 { inputs, config, lib, pkgs, ... }: {
 
   imports = [ 
-    ./hardware-configuration.nix 
+    ./linode-configuration.nix 
     ../shared/system 
   ];
 
-  # Enable linode config
-  linode.enable = true;
+  # Enable secrets
+  secrets.enable = true;
 
-  # root is tmpfs
-  fileSystems."/" = { 
-    # device = "none"; fsType = "tmpfs";
-    options = [ "size=2G" "mode=755" ]; # limit to 2GB and only writable by root
-  };
-
-  # /nix is btrfs
-  fileSystems."/nix" = { 
-    # device = "/dev/disk/by-uuid/xxx"; fsType = "btrfs";
-    options = [ "compress-force=zstd" "noatime" ]; # btrfs mount options
-    # options = [ "compress=zstd" "noatime" "space_cache=v2" "discard=async" "subvol=nix" ];
-    neededForBoot = true; 
-  };
-
-  # desktops.gnome.enable = true;
-
+  # Services
   services.tailscale.enable = true;
   services.openssh.enable = true;
-  programs.mosh.enable = true;
 
-  # services.keyd.enable = true;
-  
+  # Programs
+  programs.mosh.enable = true;
+  programs.neovim.enable = true;
+
   # services.traefik.enable = true;
-  # # services.whoogle.enable = true;
+  # services.whoogle.enable = true;
   # services.whoami.enable = true;
 
-  programs.neovim.enable = true;
 
   # # Flatpak
   # services.flatpak.enable = true;
@@ -49,12 +34,6 @@
   # services.gitea.enable = true;
   # services.gitea.database.type = "mysql";
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
-  services.fprintd.enable = true;
 
   # # Steam
   # programs.steam.enable = false;
--- hosts/sol/home.nix
+++ hosts/sol/home.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }: {
 
-  imports = [ ../. ];
+  imports = [ ../shared/user ];
 
   # ---------------------------------------------------------------------------
   # Home Enviroment & Packages
