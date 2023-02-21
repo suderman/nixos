f313d3fa08963d63dc52df79f7350195f954fde4
Jon Suderman <jon@suderman.net>
2023-02-21 11:20:36 -0700

    update

--- configurations/cog/configuration.nix
+++ configurations/cog/configuration.nix
@@ -1,4 +1,4 @@
-{ inputs, config, lib, pkgs, ... }: {
+{ config, lib, pkgs, inputs, ... }: {
 
   imports = [ 
     ./hardware-configuration.nix 
@@ -14,73 +14,58 @@
   # /nix is btrfs
   fileSystems."/nix" = { 
     # device = "/dev/disk/by-uuid/xxx"; fsType = "btrfs";
-    options = [ "compress-force=zstd" "noatime" ]; # btrfs mount options
-    # options = [ "compress=zstd" "noatime" "space_cache=v2" "discard=async" "subvol=nix" ];
+    options = [ "compress=zstd" "space_cache=v2" "discard=async" "noatime" ]; # btrfs mount options
     neededForBoot = true; 
   };
-
-  base.enable = true;
-  state.enable = true;
-  secrets.enable = true;
-
-  desktops.gnome.enable = true;
-
-  services.tailscale.enable = true;
-  services.ddns.enable = true;
-  services.openssh.enable = true;
-  programs.mosh.enable = true;
-
-  services.keyd.enable = true;
-  services.ydotool.enable = true;
-
-  services.earlyoom.enable = true;
-  
-  services.traefik.enable = true;
-  # services.whoogle.enable = true;
-  services.whoami.enable = true;
-
-  programs.neovim.enable = true;
-
-  # Flatpak
-  services.flatpak.enable = true;
-
-  # SabNZBd
-  services.sabnzbd.enable = false;
-
-  # https://search.nixos.org/options?show=services.tandoor-recipes.enable&query=services.tandoor-recipes
-  services.tandoor-recipes.enable = false;
-
-  # https://search.nixos.org/options?show=services.gitea.enable&query=services.gitea
-  # services.gitea.enable = true;
-  # services.gitea.database.type = "mysql";
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Use freshest kernel
   boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Base configuration
+  base.enable = true;
+  state.enable = true;
+  secrets.enable = true;
+
+  # Desktop Environments
+  desktops.gnome.enable = true;
+
+  # Network
+  services.tailscale.enable = true;
+  services.ddns.enable = true;
+  services.openssh.enable = true;
+  networking.extraHosts = "";
+
+  # Memory management
+  services.earlyoom.enable = true;
+
+  # Fingerprint reader
   services.fprintd.enable = true;
 
-  # Steam
-  programs.steam.enable = false;
+  # Keyboard control
+  services.keyd.enable = true;
+  services.ydotool.enable = true;
 
+  # Database services
   services.mysql.enable = true;
-  # services.postgresql.enable = true;
+  services.postgresql.enable = false;
+  
+  # Web services
+  services.traefik.enable = true;
+  services.whoogle.enable = false;
+  services.whoami.enable = true;
+  services.sabnzbd.enable = false;
+  services.tandoor-recipes.enable = false;
+  # services.gitea.enable = true;
+  # services.gitea.database.type = "mysql";
 
-  # programs._1password.enable = true;
-  # programs._1password-gui.polkitPolicyOwners = [ "me" ];
-  # programs._1password-gui.enable = true;
-
-  # Packages
-  # environment.systemPackages = with pkgs; [];
-
-  # Other
-  # programs.nix-ld.enable = true;
-
-  networking.extraHosts =
-  ''
-    # 174.138.112.193 redarrow.ca www.redarrow.ca
-    # 174.138.112.193 myebus.ca www.myebus.ca
-  '';
+  # Apps
+  services.flatpak.enable = true;
+  programs.mosh.enable = true;
+  programs.neovim.enable = true;
+  programs.steam.enable = false;
 
 }
