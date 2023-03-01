79e026fc5f804cbe55e40b5cf04b785e4acfd43a
Jon Suderman <jon@suderman.net>
2023-03-01 10:20:09 -0700

    save

--- configurations/sol/configuration.nix
+++ configurations/sol/configuration.nix
@@ -1,8 +1,51 @@
 { inputs, config, lib, pkgs, ... }: {
 
-  imports = [ 
-    ./hardware-configuration.nix 
+  imports = [
+    ./hardware-configuration.nix
   ];
+
+  fileSystems."/".options = [ "compress=zstd" "space_cache=v2" "discard=async" "noatime" "subvol=root" ];
+  fileSystems."/nix".options = [ "compress=zstd" "space_cache=v2" "discard=async" "noatime" ];
+
+  # boot.initrd.postDeviceCommands = lib.mkBefore ''
+  #   btrfs subvolume list -o /nix/root |
+  #   cut -f9 -d' ' |
+  #   while read subvolume; do
+  #     echo "deleting /$subvolume subvolume..."
+  #     btrfs subvolume delete "/nix/$subvolume"
+  #   done &&
+  #
+  #   echo "deleting /root subvolume..." &&
+  #   btrfs subvolume delete /nix/root
+  #
+  #   echo "restoring blank /root subvolume..."
+  #   btrfs subvolume snapshot /nix/snaps/root /nix/root
+  # '';
+
+  boot.initrd.postDeviceCommands = lib.mkBefore ''
+    # Mount btrfs disk to /mnt
+    mkdir -p /mnt
+    mount /dev/disk/by-label/Butter /mnt
+
+    # Delete all of root's subvolumes
+    btrfs subvolume list -o /mnt/root |
+    cut -f9 -d' ' |
+    while read subvolume; do
+      echo "deleting /$subvolume subvolume..."
+      btrfs subvolume delete "/mnt/$subvolume"
+    done &&
+
+    # Delete root itself
+    echo "deleting /root subvolume..." &&
+    btrfs subvolume delete /mnt/root
+
+    # Restore root from blank snapshot
+    echo "restoring blank /root subvolume..."
+    btrfs subvolume snapshot /mnt/snaps/root /mnt/root
+
+    # Clean up
+    umount /mnt
+  '';
 
   base.enable = true;
   state.enable = true;
@@ -23,7 +66,7 @@
   # Database services
   services.mysql.enable = true;
   services.postgresql.enable = false;
-  
+
   # Web services
   services.traefik.enable = true;
   services.whoogle.enable = false;
