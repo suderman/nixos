3c4d9dce6692ca29b3c60c6aec56a692ff9baff1
Jon Suderman <jon@suderman.net>
2023-03-03 12:49:14 -0700

    update

--- overlays/pkgs/nixos-cli/default.nix
+++ overlays/pkgs/nixos-cli/default.nix
@@ -1,12 +1,42 @@
-{ lib, writeShellApplication, fzf, curl, less, gnused }: (writeShellApplication {
+# { lib, writeShellApplication, fzf, curl, less, gnused }: (writeShellApplication {
+#   name = "nixos";
+#   runtimeInputs = [ fzf curl less gnused ];
+#   text = builtins.readFile ./nixos;
+#   checkPhase = false;
+# }) // {
+#   meta = with lib; {
+#     description = "nixos-cli script";
+#     license = licenses.mit;
+#     platforms = platforms.all;
+#   };
+# }
+{ lib, runtimeShell, writeTextFile, fzf, curl, less, gnused }: 
+
+let 
   name = "nixos";
+  description = "nixos-cli script";
   runtimeInputs = [ fzf curl less gnused ];
   text = builtins.readFile ./nixos;
-  checkPhase = false;
-}) // {
+in 
+
+writeTextFile {
+  inherit name;
+  executable = true;
+  destination = "/bin/${name}";
+
+  text = ''
+    #!${runtimeShell}
+  '' + lib.optionalString (runtimeInputs != [ ]) ''
+    export PATH="${lib.makeBinPath runtimeInputs}:$PATH"
+  '' + ''
+    ${text}
+  '';
+
   meta = with lib; {
-    description = "nixos-cli script";
+    mainProgram = name;
+    description = description;
     license = licenses.mit;
     platforms = platforms.all;
   };
+
 }
