4289640ef0ece7f3ff363e8501825b0a8e08eadd
Jon Suderman <jon@suderman.net>
2023-03-03 15:28:41 -0700

    bashly

--- overlays/pkgs/nixos-cli/src/bashly_generate_command.sh
+++ overlays/pkgs/nixos-cli/src/bashly_generate_command.sh
@@ -1,4 +1,4 @@
-bashly="docker run --rm -it --user $(id -u):$(id -g) --volume $PWD:/app dannyben/bashly"
+bashly="docker run --rm -it --user $(id -u):$(id -g) --volume /etc/nixos/overlays/pkgs/nixos-cli:/app dannyben/bashly"
 task -d "cd /etc/nixos/overlays/pkgs/nixos-cli && bashly generate"
 (cd /etc/nixos/overlays/pkgs/nixos-cli && $bashly generate)
 
