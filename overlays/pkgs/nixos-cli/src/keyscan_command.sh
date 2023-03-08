6c3790975d83c2b745a7822af7d6ec74e46784c1
Jon Suderman <jon@suderman.net>
2023-03-08 16:29:39 -0700

    nixos-cli

--- overlays/pkgs/nixos-cli/src/keyscan_command.sh
+++ overlays/pkgs/nixos-cli/src/keyscan_command.sh
@@ -1,4 +1,62 @@
-echo "# this file is located in 'src/keyscan_command.sh'"
-echo "# code for 'nixos keyscan' goes here"
-echo "# you can edit it freely and regenerate (it will not be overwritten)"
-inspect_args
+# inspect_args
+local dir="/etc/nixos/secrets" ip hostname force filename key
+
+function main {
+
+  # Ensure ssh-keyscan
+  has ssh-keyscan || error "ssh-keyscan missing"
+
+  # Set up args
+  force="${args[--force]}"
+  ip="${args[host]}"
+  hostname="${args[name]}"
+  [[ -z "$hostname" ]] && hostname="$ip"
+
+  # Modify hostname to ensure 
+  # - it doesn't end in .pub
+  # - periods replaces with hypens
+  # - only alphanumeric with hypens and underscores
+  # - no leading/trailing hypens and underscores
+  hostname="$(awk '{ 
+    gsub(/\.pub$/, "", $0); 
+    gsub(/[\. -]+/, "-", $0); 
+    gsub(/[^a-zA-Z0-9_-]+/, "", $0); 
+    gsub(/^[-_]+|[-_]+$/, "", $0); 
+    print tolower($0) 
+  }' <<< $hostname)"
+
+  # Attempt to scan public key
+  task ssh-keyscan -t ssh-ed25519 $ip 2> /dev/null
+  key="$(last | awk '{print $2} {print $3}' | xargs)"
+  filename="${dir}/keys/${hostname}.pub"
+
+  show "key=\"$key\""
+  if [[ -z "$key" ]]; then
+    error "failed to scan key from $ip"
+  fi
+
+  # Ask to delete existing key
+  if [[ "$force" == "1" ]]; then
+    task "rm -f $filename"
+  fi
+
+  if [[ -e $filename ]]; then
+    if ask "Overwrite existing \"$hostname\" key?"; then
+      task "rm -f $filename"
+    else
+      error "$hostname already exists in keys directory"
+    fi
+  fi
+
+  # Write key to file
+  show "echo \"\$key\" > $filename"
+  echo "$key" > $filename
+  info "Success: $name written to $filename"
+
+  # Rekey the secrets with the new identity
+  nixos rekey
+
+}
+
+
+main
--- overlays/pkgs/nixos-cli/src/secrets_command.sh
+++ overlays/pkgs/nixos-cli/src/secrets_command.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+# inspect_args
 local dir="/etc/nixos/secrets" secret 
 
 function main {
