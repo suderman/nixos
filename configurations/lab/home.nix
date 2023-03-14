6975678dd1490f66300ceceecf4b1671a0bb5878
Jon Suderman <jon@suderman.net>
2023-03-14 16:12:29 -0600

    lab

--- configurations/lab/home.nix
+++ configurations/lab/home.nix
@@ -1,11 +1,22 @@
 { config, lib, pkgs, ... }: {
 
   base.enable = true;
-  secrets.enable = false;
+  secrets.enable = true;
 
   home.packages = with pkgs; [ 
     neofetch
     yo
   ];
+
+  programs = {
+    # neovim.enable = true;
+    chromium.enable = true;
+    git.enable = true;
+    tmux.enable = true;
+    wezterm.enable = true;
+    kitty.enable = true;
+    zsh.enable = true;
+    dconf.enable = true;
+  };
 
 }
