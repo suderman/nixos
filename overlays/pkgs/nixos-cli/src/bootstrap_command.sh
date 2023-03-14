9937d4cd8ba79c8db51031f4ebdfde179f6bd82d
Jon Suderman <jon@suderman.net>
2023-03-14 16:46:29 -0600

    git

--- overlays/pkgs/nixos-cli/src/bootstrap_command.sh
+++ overlays/pkgs/nixos-cli/src/bootstrap_command.sh
@@ -512,7 +512,7 @@ function stage2 {
     info "Copying generated hardware-configuration to $config"
     task mv -f $dir/configurations/bootstrap/hardware-configuration.nix $dir/configurations/$config/hardware-configuration.nix
     task "cd $dir; git restore configurations/bootstrap"
-    task "cd $dir/.git && chown -R 1000:100 ." && echo
+    task "chown -R 1000:100 $dir $dir/.git" && echo
 
     info "Rebuilding system to $config"
     show "nixos-rebuild switch --flake $dir\#${config}"
