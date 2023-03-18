a7b6ce7816e38ac70390ec80d076636401a00309
Jon Suderman <jon@suderman.net>
2023-03-18 09:01:10 -0600

    upgrade

--- modules/nixos/flatpak/default.nix
+++ modules/nixos/flatpak/default.nix
@@ -20,7 +20,7 @@ in {
     };
 
     xdg.portal.enable = true;
-    xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    # xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
 
   };
 
