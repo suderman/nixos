9feca4edb822e83ac70422afeabf842ed612293d
Jon Suderman <jon@suderman.net>
2023-04-06 02:16:49 -0600

    lux ssd

--- configurations/lux/additional-storage.nix
+++ configurations/lux/additional-storage.nix
@@ -12,12 +12,12 @@ in {
   fileSystems = {
 
     # Additional data disk
-    "/mnt/ssd" = btrfs // { device = "/dev/disk/by-uuid/xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"; };
+    "/mnt/ssd" = btrfs // { device = "/dev/disk/by-uuid/7b44d127-729f-43fe-a809-b6aae700c1ab"; };
     "/data" = bind // { device = "/mnt/ssd/data"; };
 
-    # 4-disk RAID device 
-    "/mnt/raid" = btrfs // { device = "/dev/disk/by-uuid/75ae6de3-04d1-4c62-9d15-357038fc4d81"; };
-    "/media" = bind // { device = "/mnt/raid/media"; };
+    # # 4-disk RAID device 
+    # "/mnt/raid" = btrfs // { device = "/dev/disk/by-uuid/75ae6de3-04d1-4c62-9d15-357038fc4d81"; };
+    # "/media" = bind // { device = "/mnt/raid/media"; };
 
   };
 
