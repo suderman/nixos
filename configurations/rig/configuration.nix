76ee86c1b2dc1f1683f71c62a9889aba37db53b6
Jon Suderman <jon@suderman.net>
2023-06-03 16:09:57 -0600

    rig

---
+++ configurations/rig/configuration.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }: {
+
+  imports = [ 
+    ./hardware-configuration.nix
+    ./storage.nix
+  ];
+
+  modules.base.enable = true;
+  modules.secrets.enable = true;
+
+  # Use freshest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.networkmanager.enable = true;
+
+  # Memory management
+  modules.earlyoom.enable = true;
+
+  # Keyboard control
+  modules.keyd.enable = true;
+  modules.ydotool.enable = true;
+
+  # Apps
+  programs.mosh.enable = true;
+  modules.neovim.enable = true;
+
+  # Web services
+  modules.tailscale.enable = true;
+  modules.ddns.enable = true;
+  modules.whoami.enable = true;
+  modules.cockpit.enable = true;
+
+}
