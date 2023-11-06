154c1bfeea0631b8c73f611665dfa2123f2c2d4f
buxel <331779+buxel@users.noreply.github.com>
2023-11-06 16:17:33 +0100

    Rclone generalize (#2)
    add generic rcloune mount service

--- configurations/bender/configuration.nix
+++ configurations/bender/configuration.nix
@@ -22,19 +22,31 @@
   networking.extraHosts = "";
 
   
-  modules.rclone = {
+
+    # Ocis
+  modules.ocis = { 
     enable = true;
+    hostName = "cloud.pingbit.de";
+  };
+  # Ocis remote user data 
+  # NOTE: rclone does not support symlinks, which OCIS uses.
+  # modules.rclone.mounts."${config.modules.ocis.dataDir}" = {
+  # modules.rclone.mounts."/var/lib/ocis/storage/users" = {
+  #   configPath = config.age.secrets.rclone-conf.path;
+  #   remote = "azure-data:ocis-storage-user";
+  #   uid = config.ids.uids.ocis;
+  #   gid = config.ids.gids.ocis;
+  # }; 
+
+  # Immich remote data
+  modules.rclone.mounts."/mnt/photos" = {
     configPath = config.age.secrets.rclone-conf.path;
     remote = "azure-data:photos";
-    mountPath = "/mnt/photos";
-    requiredBy = ["immich.service"];
+    uid = config.ids.uids.immich;
+    gid = config.ids.gids.immich;
+    mountOpts = [ "log-level=INFO" ];
   };
-
-
-  # Web services
-  modules.ocis.enable = true;
-  modules.ocis.hostName = "cloud.pingbit.de";
-
+  # Immich 
   modules.immich = {
     enable = true;
     hostName = "photos.pingbit.de";
--- modules/rclone/default.nix
+++ modules/rclone/default.nix
@@ -1,4 +1,4 @@
-# modules.rclone.enable = true;
+# inspired by https://github.com/NixOS/nixpkgs/blob/nixos-23.05/nixos/modules/services/networking/wg-quick.nix
 { config, lib, pkgs, utils, ... }:
 
 let
@@ -6,84 +6,109 @@ let
   cfg = config.modules.rclone;
 
   inherit (config.users) user;
-  inherit (lib) mkIf mkOption mkBefore options types strings;
+  inherit (lib) mkIf mkOption mkBefore options types strings nameValuePair mapAttrs';
   inherit (builtins) toString;
   inherit (lib.strings) toInt;
   # Type for a valid systemd unit name. Needed for correctly passing "requiredBy" to "systemd.services"
   inherit (utils.systemdUtils.lib) unitNameType;
 
+  mountOpts = {
+    options = {
+      remote = mkOption {
+        type = types.str;
+        description = "Name of rclone remote defined in RClone config. Keep in mind to add the `:` after the name.";
+      };
 
-  configFile = pkgs.writeTextFile {
-    name = "rclone.conf";
-    text = lib.generators.toINI {} cfg.settings;
-    checkPhase = "${pkgs.rclone}/bin/rclone config show --config $out";
+      cacheDir = mkOption {
+        type = types.path;
+        default = "/var/rclone";
+        description = "Cache directory for rclone vfs";
+      };
+
+      configPath = mkOption { 
+        type = types.path; 
+        description = "RClone config file.";
+      };
+
+      uid = mkOption { 
+        type = types.ints.u32; 
+        default = 1000;
+        description = "Override the UID field set by the filesystem.";
+      };
+
+      gid = mkOption { 
+        type = types.ints.u32; 
+        default = 1000;
+        description = "Override the GID field set by the filesystem.";
+      };
+
+      mountOpts = mkOption {
+        type = types.listOf (types.str);
+        default = [];
+        description = "Additional paramenters passed as mount options";
+        example = ["noauto" "_netdev"];
+      };
+    };
   };
+
+  generateMount = name: values:
+    let
+      fsValue = {
+        device = values.remote;
+        fsType = "rclone";
+        options = [
+          "rw"
+          "allow_other"
+          "_netdev"
+          "noauto"
+          "x-systemd.automount"
+          # "x-systemd.idle-timeout=60"
+
+          # rclone specific
+          "env.PATH=/run/wrappers/bin" # for fusermount3
+          "config=${values.configPath}"
+          "cache_dir=${values.cacheDir}"
+          "uid=${toString values.uid}"
+          "gid=${toString values.gid}"
+          "dir-perms=770"
+          "file-perms=0664"
+          "umask=002"
+          "allow-non-empty"
+          "allow-other"
+          "vfs-cache-mode=full"
+          "vfs-cache-max-size=10G"
+        ] ++ values.mountOpts;
+      };
+    in
+    nameValuePair "${name}" fsValue;
+
 
 in {  
+
+  ###### interface
+
   options.modules.rclone = {
-
-    enable = options.mkEnableOption "rclone"; 
-
-    remote = mkOption {
-      type = types.str;
-      description = "Name of rclone remote defined in RClone config. Keep in mind to add the `:` after the name.";
-    };
-
-    mountPath = mkOption {
-      type = types.path;
-      default = "/mnt/rclone";
-      description = "Mointpoint for rclone remote";
-    };
-
-    cacheDir = mkOption {
-      type = types.path;
-      default = "/var/rclone";
-      description = "Cache directory for rclone vfs";
-    };
-
-    configPath = mkOption { 
-      type = types.path; 
-      description = "RClone config file.";
-    };
+      mounts = mkOption {
+        description = "Rcloune mounts.";
+        default = {};
+        example = {
+          "/mnt/rclone" = {
+            configPath = "/etc/rclone/rclone.conf";
+            remote = "my-remote:";
+            cacheDir = "/var/lib/rclone";
+          };
+        };
+        type = with types; attrsOf (submodule mountOpts);
+      };
   };
 
-  # TODO: allow configuration of multiple mounts via <name> like here https://github.com/NixOS/nixpkgs/blob/nixos-23.05/nixos/modules/services/networking/wg-quick.nix#L219
-  # Use this to properly escape the unit name https://github.com/NixOS/nixpkgs/blob/edbe9ad5e0a129424abdde36a0124333213fc667/nixos/lib/utils.nix#L46
-  config = mkIf cfg.enable {  
+  ###### implementation
 
-    # Name sanitazion: builtins.concatStringsSep "-" (builtins.filter (v: ! builtins.isList v) (builtins.split "[^[:alnum:]]" "azure:blob/dir"))
-    # Add user to the rclone group
-    # users.users."${user}".extraGroups = [ "rclone" ]; 
-
+  config = mkIf (cfg.mounts != {}) {
     environment.systemPackages = [ pkgs.unstable.rclone ];
     system.fsPackages = [ pkgs.unstable.rclone ];
     systemd.packages = [ pkgs.unstable.rclone ];
-    
 
-    fileSystems."${cfg.mountPath}" = {
-      device = cfg.remote;
-      fsType = "rclone";
-      options = [
-        "rw"
-        "allow_other"
-        "_netdev"
-        "noauto"
-        "x-systemd.automount"
-        "x-systemd.idle-timeout=60"
-
-        # rclone specific
-        "env.PATH=/run/wrappers/bin" # for fusermount3
-        "config=${cfg.configPath}"
-        "cache_dir=${cfg.cacheDir}"
-        "vfs-cache-mode=full"
-        # "dir-perms=770"
-        # "file-perms=0664"
-        # "umask=002"
-        # "allow-non-empty"
-        # "log-level=INFO"
-        # "vfs-cache-mode full"
-        # "vfs-cache-max-size 20G"
-      ];
-    };
-  };  
+    fileSystems = mapAttrs' generateMount cfg.mounts;
+  };
 }
