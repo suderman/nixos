94878d695b35a2dec14efcf53a1303ba0e741f2b
Jon Suderman <jon@suderman.net>
2023-11-07 22:08:05 -0700

    extend silverbullet service

--- modules/silverbullet/default.nix
+++ modules/silverbullet/default.nix
@@ -3,8 +3,14 @@
 
 let
 
+  # https://github.com/silverbulletmd/silverbullet/releases
+  version = "0.5.3";
+
   cfg = config.modules.silverbullet;
-  inherit (lib) mkIf mkOption options types strings;
+  ownership = with config.ids; "${toString uids.silverbullet}:${toString gids.silverbullet}";
+
+  inherit (config.users) user;
+  inherit (lib) mkIf mkOption options types strings mkBefore;
   inherit (builtins) toString;
 
 in {
@@ -19,22 +25,71 @@ in {
       description = "FQDN for the SilverBullet instance";
     };
 
+    dataDir = mkOption {
+      type = types.path;
+      default = "/var/lib/silverbullet";
+    };
+
   };
 
   config = mkIf cfg.enable {
+
+    # Unused uid/gid snagged from this list:
+    # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/misc/ids.nix
+    ids.uids.silverbullet = 913;
+    ids.gids.silverbullet = 913;
+
+    users.users.silverbullet = {
+      isSystemUser = true;
+      group = "silverbullet";
+      description = "silverbullet daemon user";
+      home = cfg.dataDir;
+      uid = config.ids.uids.silverbullet;
+    };
+
+    users.groups.silverbullet = {
+      gid = config.ids.gids.silverbullet;
+    };
+
+    # Add user to the silverbullet group
+    users.users."${user}".extraGroups = [ "silverbullet" ]; 
 
     # Enable reverse proxy
     modules.traefik.enable = true;
 
     virtualisation.oci-containers.containers.silverbullet = {
-      image = "zefhemel/silverbullet:0.5.3";
-      volumes = [ "space:/space" ];
+      image = "zefhemel/silverbullet:${version}";
+      autoStart = true;
+
+      # Run as silverbullet user
+      user = ownership;
+
+      # Traefik labels
       extraOptions = [
         "--label=traefik.enable=true"
         "--label=traefik.http.routers.silverbullet.rule=Host(`${cfg.hostName}`)"
         "--label=traefik.http.routers.silverbullet.tls.certresolver=resolver-dns"
         "--label=traefik.http.routers.silverbullet.middlewares=local@file"
       ];
+
+      volumes = [ "${cfg.dataDir}:/space" ];
+
+    };
+
+    # Extend systemd service
+    systemd.services.docker-silverbullet = {
+
+      preStart = mkBefore ''
+        mkdir -p ${cfg.dataDir}
+        chown -R ${ownership} ${cfg.dataDir}
+      '';
+
+      # traefik should be running before this service starts
+      after = [ "traefik.service" ];
+
+      # If the proxy goes down, take down this service too
+      requires = [ "traefik.service" ];
+
     };
 
   };
