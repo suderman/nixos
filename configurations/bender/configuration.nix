55830feb2c6539b63875f2beddce4da90a8c51ad
buxel <buxel.dev@gmail.com>
2023-11-26 16:49:28 +0100

    move mounts to storage

--- configurations/bender/configuration.nix
+++ configurations/bender/configuration.nix
@@ -25,48 +25,7 @@
     hostName = "cloud.pingbit.de";
     # dataDir = "/mnt/ocis";
   };
-
-  # Ocis remote user data 
-  # NOTE: rclone does not support symlinks, which OCIS uses.
-  modules.blobfuse.package = pkgs.unstable.blobfuse;
-  
-  # Workarounds
-  age.secrets.blobfuse-yaml.name = "blobfuse.yaml"; # blobfuse inists on a ".yaml extension. This messes up secrets handling with "nixos secrets", as dots indicate a hierarchy level in nix.
-  # TODO: maybe also inject RequiresMountsFor into systemd unit: https://www.freedesktop.org/software/systemd/man/latest/systemd.unit.html#RequiresMountsFor=
-  systemd.services.docker-ocis.bindsTo = [ "var-lib-ocis-storage.mount" ]; # create a dependency that also kills the service, if the mount is gone
-  # TODO: make mountpoint immutable so it is not accidently written
-
-  # TODO: wrap this in a module/overlay/whatever is a smooth way to integrate it with existing modules
-  modules.blobfuse.mounts."${config.modules.ocis.dataDir}/storage" = {
-  #modules.blobfuse.mounts."/mnt/ocis" = {
-  #modules.blobfuse.mounts."${config.modules.ocis.dataDir}" = {
-    configPath = config.age.secrets."blobfuse-yaml".path;
-    container = "ocis";
-    # mountOpts = [ "--log-level=LOG_DEBUG" ];
-    uid = config.ids.uids.ocis;
-    gid = config.ids.gids.ocis;
-  }; 
-
-  # Immich remote data
-  # modules.rclone.mounts."/mnt/photos" = {
-  #   configPath = config.age.secrets.rclone-conf.path;
-  #   remote = "azure-data:photos";
-  #   uid = config.ids.uids.immich;
-  #   gid = config.ids.gids.immich;
-  #   mountOpts = [ "log-level=INFO" ];
-  # };
-
-  systemd.services.immich.unitConfig = {
-    RequiresMountsFor = config.modules.immich.dataDir;
-  };
-  modules.blobfuse.mounts."${config.modules.immich.dataDir}" = {
-  #modules.blobfuse.mounts."/mnt/ocis" = {
-  #modules.blobfuse.mounts."${config.modules.ocis.dataDir}" = {
-    configPath = config.age.secrets."blobfuse-yaml".path;
-    container = "photos";
-    uid = config.ids.uids.immich;
-    gid = config.ids.gids.immich;
-  }; 
+ 
   # Immich 
   modules.immich = {
     enable = true;
@@ -78,31 +37,6 @@
   modules.silverbullet.hostName = "wiki.pingbit.de";
   
   modules.netdata.enable = true;
-
-  ## Testing
-
-  # systemd.mounts = [{
-  #   enable = true;
-  #   description = "blobfuse mount test";
-  #   after = [ "network-online.target" ];
-  #   requires = [ "network-online.target" ];
-  #   wantedBy = [ "multi-user.target" ];
-  #   what = "${pkgs.unstable.blobfuse}/bin/azure-storage-fuse"; #"azure-storage-fuse";
-  #   where = "/var/lib/ocis";
-  #   type = "fuse3";
-  #   mountConfig = {
-  #     SloppyOptions = true;
-  #   };
-  #   options = "defaults,_netdev,allow_other,--config-file=/home/me/blob-ocis.yaml"; 
-  # }]; 
-
-  # systemd.automounts = [{
-  #   enable = true;
-  #   after = [ "network-online.target" ];
-  #   before = [ "remote-fs.target" ];
-  #   where = "/mnt/azblob";
-  #   wantedBy = [ "multi-user.target" ];
-  # }];
 }
 
 # Ideas / todos:
---
+++ configurations/bender/storage.nix
@@ -0,0 +1,49 @@
+{ config, ... }: let 
+
+  automount = [ 
+    "noauto"                       # do not mount on boot
+    "nofail"                       # continue boot even if disk is missing
+    "x-systemd.automount"          # create automount unit to mount when accessed
+    "x-systemd.device-timeout=1ms" # assume device is already plugged in and do not wait
+    "x-systemd.idle-timout=5m"     # unmount after 5 min of inactivity
+  ];
+  btrfs = [ 
+    "compress=zstd"                # enable zstd compression
+    "space_cache=v2"               # track available free space on filesystem
+    "discard=async"                # free up deleted space in the background
+    "noatime"                      # disables access time updates on files
+  ]; 
+  bind = [ "bind" ]; 
+
+in {
+
+  # Btrfs mount options
+  fileSystems."/".options = btrfs;
+  fileSystems."/nix".options = btrfs;
+
+  # Blobfuse mounts
+  # Workarounds
+  modules.blobfuse.package = pkgs.unstable.blobfuse;
+  age.secrets.blobfuse-yaml.name = "blobfuse.yaml"; # blobfuse inists on a ".yaml extension. This messes up secrets handling with "nixos secrets", as dots indicate a hierarchy level in nix.
+
+  # TODO: wrap this in a module/overlay/whatever is a smooth way to integrate it with existing modules
+  # TODO: make mountpoint immutable so it is not accidently written
+
+  modules.blobfuse.mounts."${config.modules.ocis.dataDir}/storage" = {
+    configPath = config.age.secrets."blobfuse-yaml".path;
+    container = "ocis";
+    uid = config.ids.uids.ocis;
+    gid = config.ids.gids.ocis;
+  }; 
+
+  systemd.services.immich.unitConfig = {
+    RequiresMountsFor = config.modules.immich.dataDir;
+  };
+  modules.blobfuse.mounts."${config.modules.immich.dataDir}" = {
+    configPath = config.age.secrets."blobfuse-yaml".path;
+    container = "photos";
+    uid = config.ids.uids.immich;
+    gid = config.ids.gids.immich;
+  }; 
+
+}
