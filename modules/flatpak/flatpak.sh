f6baf5602a0d8254f6febf80cf95f3864c8864e5
Jon Suderman <jon@suderman.net>
2023-12-05 17:03:27 -0700

    flatpak fix

--- modules/flatpak/flatpak.sh
+++ modules/flatpak/flatpak.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Wait until network is up
-until /run/wrappers/bin/ping dl.flathub.org -c1 -q >/dev/null; do :; done
+until ping dl.flathub.org -c1 -q >/dev/null; do :; done
 
 # Add flathub remotes if they doesn't yet exist
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
