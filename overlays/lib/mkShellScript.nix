1ef32be8896da5fcd490a73fb85d597391e2a451
Jon Suderman <jon@suderman.net>
2023-12-05 17:03:43 -0700

    mkShellScript

---
+++ overlays/lib/mkShellScript.nix
@@ -0,0 +1,25 @@
+# mkShellScript
+{ pkgs, lib, this }:
+
+  { name, text, inputs ? [] }: pkgs.writeTextFile {
+
+    inherit name;
+    executable = true;
+    destination = "/bin/${name}";
+
+    text = ''
+      #!${pkgs.runtimeShell}
+    '' + lib.optionalString (inputs != []) ''
+      export PATH="${lib.makeBinPath inputs}:$PATH"
+    '' + ''
+      ${text}
+    '';
+
+    meta = with lib; {
+      mainProgram = name;
+      description = "Personal shell script";
+      license = licenses.mit;
+      platforms = platforms.all;
+    };
+
+  }
