e2c9f02d37b560687a470a3bdc687af7d207dd30
Jon Suderman <jon@suderman.net>
2024-01-07 11:08:42 -0700

    clean secrets

--- secrets/files/default.nix
+++ secrets/files/default.nix
@@ -7,7 +7,6 @@
   basic-auth = ./basic-auth.age;
   btrbk-key = ./btrbk-key.age;
   cloudflare-env = ./cloudflare-env.age;
-  immich-env = ./immich-env.age;
   ocis-env = ./ocis-env.age;
   password = ./password.age;
   password-hash = ./password-hash.age;
