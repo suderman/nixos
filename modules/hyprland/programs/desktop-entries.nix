dcfa3d9ccd9cdd46b60e90bfd71a162693658317
Jon Suderman <jon@suderman.net>
2024-06-30 10:46:28 -0600

    desktop-entries

--- modules/hyprland/programs/desktop-entries.nix
+++ modules/hyprland/programs/desktop-entries.nix
@@ -1,23 +1,52 @@
-{ config, lib, pkgs, ... }: {
-  config = lib.mkIf config.wayland.windowManager.hyprland.enable {
+{ config, lib, pkgs, ... }: let
 
-    xdg.desktopEntries = {
+  inherit (lib) mkIf removePrefix removeSuffix;
+  urlToClass = url: removeSuffix "/" (removePrefix "http://" (removePrefix "https://" url));
+
+  mkPWA = { name, icon, url }: {
+    "chrome-${urlToClass url}__-Default" = {
+      inherit name icon;
+      exec = "chromium --ozone-platform-hint=auto --force-dark-mode --enable-features=WebUIDarkMode --app=\"${url}/\" %U";
+    };
+  };
+
+  mkHidden = { name, icon, class }: {
+    "${class}" = {
+      inherit name icon;
+      noDisplay = true;
+    };
+  }; 
+
+in {
+  config = mkIf config.wayland.windowManager.hyprland.enable {
+    xdg.desktopEntries = {} //
 
       # Sushi (Quick Look)
-      "org.gnome.NautilusPreviewer" = {
-        name = "Sushi";
-        icon = "image-viewer";
-        noDisplay = true;
-      };
+      mkHidden {
+        name = "Sushi"; 
+        icon = "image-viewer"; 
+        class = "org.gnome.NautilusPreviewer";
+      } //
 
       # GIMP
-      "gimp-2.99" = {
-        name = "GIMP";
-        icon = "org.gimp.GIMP";
-        noDisplay = true;
-      };
+      mkHidden {
+        name = "GIMP"; 
+        icon = "org.gimp.GIMP"; 
+        class = "gimp-2.99";
+      } //
 
-    };
+      mkPWA { 
+        name = "Immich"; 
+        icon = "image-viewer"; 
+        url = "https://immich.lux";
+      } //
 
+      mkPWA { 
+        name = "SilverBullet"; 
+        icon = "image-viewer"; 
+        url = "https://silverbullet.lux";
+      } //
+
+    {};
   };
 }
