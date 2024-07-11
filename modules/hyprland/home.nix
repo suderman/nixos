40180ddd47756ff1ab2c9fd3f00a43eeed402493
Jon Suderman <jon@suderman.net>
2024-07-11 09:26:31 -0600

    hyprland

--- modules/hyprland/home.nix
+++ modules/hyprland/home.nix
@@ -3,7 +3,7 @@
 
   cfg = config.wayland.windowManager.hyprland;
   oscfg = osConfig.programs.hyprland;
-  inherit (lib) concatStringsSep ls mkIf mkMerge mkOption mkShellScript removeSuffix types;
+  inherit (lib) ls mkIf mkMerge mkOption removeSuffix types;
 
 in {
 
@@ -20,58 +20,15 @@ in {
 
   config = mkIf oscfg.enable {
 
+    # I'll never remember the H
+    home.shellAliases = {
+      hyprland = "Hyprland";
+    };
+
     # Add target that is enabled by exec-once at the top of the configuration
     systemd.user.targets."${removeSuffix ".target" cfg.systemd.target}".Unit = {
       Description = "Hyprland compositor session after dbus-update-activation-environment";
       Requires = [ "hyprland-session.target" ];
-    };
-
-    # Add these to my path
-    home.packages = let
-
-      # Ensure portals and other systemd user services are running
-      # https://wiki.hyprland.org/Useful-Utilities/xdg-desktop-portal-hyprland/
-      bounce = mkShellScript {
-        inputs = with pkgs; [ systemd ]; name = "bounce"; text = let 
-          restart = name: "sleep 1 && systemctl --user stop ${name} && systemctl --user start ${name}";
-        in concatStringsSep "\n" [ 
-
-          # Ensure portals and other systemd user services are running
-          # https://wiki.hyprland.org/Useful-Utilities/xdg-desktop-portal-hyprland/
-          ( restart "xdg-desktop-portal-hyprland" )
-          ( restart "xdg-desktop-portal-gtk" )
-          ( restart "xdg-desktop-portal" )
-          ( restart "hyprland-ready.target" )
-
-        ];
-      };
-
-    in with pkgs; [ 
-      bounce
-      inputs.hyprswitch.packages."${pkgs.stdenv.system}".default
-      brightnessctl
-      # pamixer
-      # ncpamixer
-      font-awesome
-      wezterm
-      swww
-      wlogout # logout menu
-      wl-clipboard # copying and pasting
-      hyprpicker  # color picker
-      hyprcursor
-      wf-recorder # screen recording
-      grim # taking screenshots
-      slurp # selecting a region to screenshot
-    ];
-
-    programs = {
-      kitty.enable = true;
-      chromium.enable = true;
-    };
-
-    # I'll never remember the H
-    home.shellAliases = {
-      hyprland = "Hyprland";
     };
 
     # Automatically enable home-manager module if nixos module is enabled
---
+++ modules/hyprland/programs/programs.nix
@@ -0,0 +1,50 @@
+{ config, lib, pkgs, ... }: let
+
+  inherit (lib) concatStringsSep ls mkIf mkShellScript;
+
+  # Ensure portals and other systemd user services are running
+  # https://wiki.hyprland.org/Useful-Utilities/xdg-desktop-portal-hyprland/
+  bounce = mkShellScript {
+    inputs = with pkgs; [ systemd ]; name = "bounce"; text = let 
+      restart = name: "sleep 1 && systemctl --user stop ${name} && systemctl --user start ${name}";
+    in concatStringsSep "\n" [ 
+
+      # Ensure portals and other systemd user services are running
+      # https://wiki.hyprland.org/Useful-Utilities/xdg-desktop-portal-hyprland/
+      ( restart "xdg-desktop-portal-hyprland" )
+      ( restart "xdg-desktop-portal-gtk" )
+      ( restart "xdg-desktop-portal" )
+      ( restart "hyprland-ready.target" )
+
+    ];
+  };
+
+in {
+
+  # Programs and packages required by my Hyprland
+  config = mkIf config.wayland.windowManager.hyprland.enable {
+
+    # Check modules directory for extra configuration
+    programs = {
+      kitty.enable = true;
+      chromium.enable = true;
+    };
+
+    # Add these to my path
+    home.packages = with pkgs; [ 
+      bounce # defined above
+      swww # wallpaper
+      brightnessctl
+      font-awesome
+      wlogout # logout menu
+      wl-clipboard # copying and pasting
+      hyprpicker  # color picker
+      hyprcursor
+      wf-recorder # screen recording
+      grim # taking screenshots
+      slurp # selecting a region to screenshot
+    ];
+
+  };
+
+}
