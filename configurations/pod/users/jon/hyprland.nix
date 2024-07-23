ed1c1e2af967bd01cbf10f9c489a793a7178d3fd
Jon Suderman <jon@suderman.net>
2024-07-23 05:28:59 -0600

    pod

--- configurations/pod/users/jon/hyprland.nix
+++ configurations/pod/users/jon/hyprland.nix
@@ -1,5 +1,9 @@
 { config, lib, pkgs, this, ... }: {
 
-  config.wayland.windowManager.hyprland.settings = {};
+  config.wayland.windowManager.hyprland = {
+    settings = {};
+    extraSinks = [ "bluez_output.AC_3E_B1_9F_43_35.1" ]; # pixel buds pro
+    hiddenSinks = [];
+  };
 
 }
