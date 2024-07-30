be24cfcb799c222e6545b0710246ac6ee6b4c034
Jon Suderman <jon@suderman.net>
2024-07-30 06:13:37 -0600

    home

--- configurations/cog/users/jon/home.nix
+++ configurations/cog/users/jon/home.nix
@@ -8,12 +8,12 @@ in {
   xdg.userDirs = with config.home; {
     enable = true;
     createDirectories = false;
-    download = "${homeDirectory}/tmp";
-    desktop = "${homeDirectory}/data";
-    documents = "${homeDirectory}/data/documents";
-    music = "${homeDirectory}/data/music";
-    pictures = "${homeDirectory}/data/images";
-    videos = "${homeDirectory}/data/videos";
+    download = "${homeDirectory}/Downloads";
+    desktop = "${homeDirectory}/Personal";
+    documents = "${homeDirectory}/Personal/documents";
+    music = "${homeDirectory}/Personal/music";
+    pictures = "${homeDirectory}/Personal/images";
+    videos = "${homeDirectory}/Personal/videos";
     # publicShare = "${homeDirectory}/public";
   };
 
