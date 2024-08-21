9c3e5f7ae3102d35957e2bc7c3209125beafc06a
Jon Suderman <jon@suderman.net>
2024-08-21 10:35:01 -0600

    restore impermanence to master branch

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
 
     # Persist state
     # <https://github.com/nix-community/impermanence>
-    impermanence.url = "github:nix-community/impermanence/23c1f06316";
+    impermanence.url = "github:nix-community/impermanence";
 
     # NixOS & Home Manager secrets
     # <https://github.com/ryantm/agenix>
