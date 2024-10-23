8dcc902b56bf2c52d1733f5d5581262345b9a466
Jon Suderman <jon@suderman.net>
2024-10-23 16:46:28 -0600

    Unhide optical audio

--- configurations/kit/users/jon/hyprland.nix
+++ configurations/kit/users/jon/hyprland.nix
@@ -33,7 +33,7 @@
 
   programs.rofi = {
     extraSinks = [ "bluez_output.AC_3E_B1_9F_43_35.1" ]; # pixel buds pro
-    hiddenSinks = [ "alsa_output.usb-Generic_USB_Audio-00.HiFi__SPDIF__sink" ];
+    # hiddenSinks = [ "alsa_output.usb-Generic_USB_Audio-00.HiFi__SPDIF__sink" ];
   };
 
 }
