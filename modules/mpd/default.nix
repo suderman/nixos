f9832169307021a51293ed48212b63ed3158e7ef
Jon Suderman <jon@suderman.net>
2024-11-07 06:15:14 -0700

    mpd

--- modules/mpd/default.nix
+++ modules/mpd/default.nix
@@ -1,26 +1,31 @@
 { config, lib, ... }: let
 
-  inherit (lib) attrValues filterAttrs map;
+  inherit (lib) attrValues filterAttrs hasAttr map;
   mdpPort = 6600; # default port for mpd control
   httpPort = 8600; # default port for http streaming
 
 in {
 
-  # Open firewall for user mpd service
-  config.networking.firewall.allowedTCPPorts = let  
+  # Check for home-manager (not-root) configurations
+  networking.firewall = if ! hasAttr "home-manager" config then {} else {
 
-    # find all home-manager users with mpd enabled
-    users = filterAttrs( _: user: (user.services.mpd.enable == true) ) config.home-manager.users; 
+    # Open firewall for user mpd service
+    allowedTCPPorts = let  
 
-    # [ 0 1 2 ... ]
-    offsets = map (user: user.home.offset) (attrValues users); 
+      # find all home-manager users with mpd enabled
+      users = filterAttrs( _: user: (user.services.mpd.enable == true) ) config.home-manager.users; 
 
-    # [ 6600 6601 6602 ... ]
-    mdpPorts = map (offset: mdpPort + offset) offsets; 
+      # [ 0 1 2 ... ]
+      offsets = map (user: user.home.offset) (attrValues users); 
 
-    # [ 8600 8601 8602 ... ]
-    httpPorts = map (offset: httpPort + offset) offsets; 
+      # [ 6600 6601 6602 ... ]
+      mdpPorts = map (offset: mdpPort + offset) offsets; 
 
-  in mdpPorts ++ httpPorts;
+      # [ 8600 8601 8602 ... ]
+      httpPorts = map (offset: httpPort + offset) offsets; 
+
+    in mdpPorts ++ httpPorts;
+
+  };
 
 }
