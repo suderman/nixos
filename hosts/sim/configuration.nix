0187ea061e9a6edc983a1be61fc125753060fba6
Jon Suderman <jon@suderman.net>
2025-03-04 07:14:08 -0700

    wip

--- hosts/sim/configuration.nix
+++ hosts/sim/configuration.nix
@@ -1,15 +1,58 @@
-{ flake, inputs, perSystem, pkgs, ... }: {
+{ flake, inputs, perSystem, pkgs, modulesPath, ... }: {
 
   imports = [
-    # flake.modules.nixos.server
+    (modulesPath + "/profiles/qemu-guest.nix")
+    (modulesPath + "/virtualisation/qemu-vm.nix")
   ];
 
+  boot.kernelParams = [ "console=ttyS0" "console=tty1" "boot.shell_on_fail" ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.initrd.checkJournalingFS = false;
+
+  nixpkgs.hostPlatform = "x86_64-linux";
+  nixpkgs.config.system-features = [ "kvm" ];
+
   environment.systemPackages = [
-    # perSystem.nixos-anywhere.default
     pkgs.vim
   ];
 
-  nixpkgs.hostPlatform = "x86_64-linux";
+  # users.users.root = {
+  #   initialPassword = "root";
+  # };
+
+  # Disallow modifying users outside of this config
+  users.mutableUsers = false;
+
+  users.users.jon = {
+    createHome = true;
+    isNormalUser = true;
+    shell = pkgs.bash;
+    uid = 1000;
+    initialPassword = "ginger";
+    group = "users";
+    extraGroups = [ "wheel" "input" ];
+    linger = true;
+  };
+
+  users.groups.users = {};
+
+  fileSystems."/" = {
+    device = "/dev/disk/by-label/nixos";
+    fsType = "ext4";
+  };
+
+  virtualisation = {
+    diskSize = 4096;   # Disk size in MB
+    memorySize = 2048; # RAM in MB
+    qemu.networkingOptions = [ "-nic bridge,br=bridge0,model=virtio-net-pci,mac=52:54:00:12:34:56" ];
+  };
+
+  networking.interfaces.enp0s1.macAddress = "52:54:00:12:34:56";
+
+  networking.firewall.allowPing = true;
+
+  services.openssh.enable = true;
 
   system.stateVersion = "24.11";
+
 }
