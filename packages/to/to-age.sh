631531fc60d4b532ee1c7ac295320582b2b40f7e
Jon Suderman <jon@suderman.net>
2025-03-13 17:02:14 -0600

    to

--- packages/to/to-age.sh
+++ packages/to/to-age.sh
@@ -3,8 +3,7 @@ input="$(cat)"
 [[ -z "$input" ]] && exit 0
 
 # Use to-ssh (this flake) to generate ssh key from input
-# ssh="$(echo "$input" | to-ssh)"
-ssh="$(echo "$input" | bash -c "$0 ssh")"
+ssh="$(echo "$input" | $0 ssh)"
 [[ -z "$ssh" ]] && exit 0
 
 # Use https://github.com/Mic92/ssh-to-age to generate age from ssh key
@@ -12,9 +11,6 @@ age="$(echo "$ssh" | ssh-to-age -private-key)"
 [[ -z "$age" ]] && exit 0
 
 # Use to-public (this flake) to generate formatted identity and output
-# echo "# imported from: $(echo "$ssh" | to-public)"
-# echo "# public key: $(echo "$age" | to-public)"
-# echo "$age"
-echo "# imported from: $(echo "$ssh" | bash -c "$0 public")"
-echo "# public key: $(echo "$age" | bash -c "$0 public")"
+echo "# imported from: $(echo "$ssh" | $0 public)"
+echo "# public key: $(echo "$age" | $0 public)"
 echo "$age"
