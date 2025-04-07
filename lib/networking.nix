3f2d3196c65be43bba0a3744c5207a10f3579b19
Jon Suderman <jon@suderman.net>
2025-04-07 16:55:44 -0600

    networking

--- lib/networking.nix
+++ lib/networking.nix
@@ -4,6 +4,15 @@
   inherit (flake.lib) mkAttrs;
   inherit (lib) foldl filterAttrs mapAttrsToList mapAttrs' nameValuePair; 
 
+  # Centralized hierarchy of IP addresses
+  zones = mkAttrs ../zones (zone: import ../zones/${zone});
+
+  # Determine primary IP address for each host from configuration domain
+  addresses = filterAttrs (n: v: v != "") ( mkAttrs flake.nixosConfigurations ( hostName: let
+    inherit (flake.nixosConfigurations."${hostName}".config.networking) domain;
+    ip = zones.${domain}.${hostName} or "";
+  in ip ) );
+
   # Flatten the tree into a "hostName.domain = address" set
   flatten = tree: foldl (a: b: a // b) {}  ( 
     mapAttrsToList (domain: hostNames: 
@@ -15,17 +24,11 @@ in rec {
   # Self-signed CA certificate, domain name used for public services
   inherit (import ../zones) ca domainName;
 
-  # Centralized list of IP addresses
-  zones = mkAttrs ../zones (zone: import ../zones/${zone});
+  # Centralized hierarchy of IP addresses
+  inherit zones;
 
   # Internal DNS records
-  records = (flatten zones) // domains;
-
-  # Determine IP address for each host from configuration domain
-  domains = filterAttrs (n: v: v != "") ( mkAttrs flake.nixosConfigurations ( hostName: let
-    inherit (flake.nixosConfigurations."${hostName}".config.networking) domain;
-    ip = if zones ? ${domain} then ( if zones.${domain} ? ${hostName} then zones.${domain}.${hostName} else "" ) else "";
-  in ip ) );
+  records = (flatten zones) // addresses;
 
 }
 
--- modules/nixos/common/networking.nix
+++ modules/nixos/common/networking.nix
@@ -1,28 +1,48 @@
 { flake, config, lib, hostName, ... }: let
-  inherit (lib) mkDefault mkOption types;
+  inherit (builtins) attrNames attrValues filter;
+  inherit (lib) filterAttrs hasPrefix mkDefault mkOption naturalSort types unique;
 in {
 
   # Extra options for each host
   options.networking = {
 
-    addresses = mkOption {
-      description = "IP addresses associated with this host";
+    hostNames = mkOption {
+      description = "All hostnames this host can be reached at";
       type = with types; listOf str;
       default = [];
     };
-    
-    hostNames = mkOption {
-      description = "Hostnames associated with this host";
+
+    address = mkOption {
+      description = "Primary IP address this host can be reached at";
+      type = with types; str;
+      default = "127.0.0.1";
+    };
+
+    addresses = mkOption {
+      description = "All IP addresses this host can be reached at";
       type = with types; listOf str;
       default = [];
     };
 
   };
 
-  config = {
+  config.networking = {
 
-    # Derive hostName from blueprint ./hosts/dir
-    networking.hostName = hostName;
+    # Derive primary hostName from blueprint ./hosts/dir
+    hostName = hostName;
+
+    # All the hostNames this host can be reached with
+    hostNames = filter 
+      (name: hasPrefix hostName name) 
+      (attrNames flake.networking.records);
+
+    # Primary IP address from flake's zones
+    address = flake.networking.records.${hostName} or "127.0.0.1";  
+
+    # All the IP addresses this host can be reached with
+    addresses = [ "127.0.0.1" ] ++ unique( naturalSort( attrValues( 
+      filterAttrs (name: ip: hasPrefix hostName name) flake.networking.records 
+    )));
 
   };
 
