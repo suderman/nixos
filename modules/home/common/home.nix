b626a0263ad756b6c878f69e2cf476d1f7665088
Jon Suderman <jon@suderman.net>
2025-04-19 20:08:43 -0600

    home uid

---
+++ modules/home/common/home.nix
@@ -0,0 +1,39 @@
+{ config, osConfig, lib, pkgs, ... }: {
+
+  # Lookup uid from flake.users.jon.uid and assign to config.home.uid
+  options.home.uid = let 
+    inherit (lib) mkOption types;
+    inherit (config.home) username;
+  in mkOption {
+    type = with types; nullOr int;
+    default = osConfig.users.users.${username}.uid or null;
+  };
+
+  # Calculate offet added to ports (uid - 1000) and assign to config.home.offset
+  options.home.offset = let 
+    inherit (lib) mkOption types;
+    uid = if config.home.uid == null then 1000 else config.home.uid;
+  in mkOption {
+    type = with types; nullOr int;
+    default = if uid >= 1000 then uid - 1000 else 0;
+  };
+
+  # ---------------------------------------------------------------------------
+  # User Configuration
+  # ---------------------------------------------------------------------------
+  config = {
+
+    # Add support for ~/.local/bin
+    home.sessionPath = [ "$HOME/.local/bin" ];
+
+    # Additional env variables
+    home.sessionVariables = {
+
+      # Accept agreements for unfree software (when installing impertively)
+      NIXPKGS_ALLOW_UNFREE = "1";
+
+    };
+
+  };
+
+}
