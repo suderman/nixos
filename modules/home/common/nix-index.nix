5a287613709f572fc30157286e764969663ad509
Jon Suderman <jon@suderman.net>
2025-07-14 18:18:33 -0600

    hmModules -> homeModules

--- modules/home/common/nix-index.nix
+++ modules/home/common/nix-index.nix
@@ -1,16 +1,20 @@
-{ inputs, lib, ... }: {
-
+{
+  inputs,
+  lib,
+  ...
+}: {
   imports = [
-    inputs.nix-index-database.hmModules.nix-index
+    inputs.nix-index-database.homeModules.nix-index
   ];
 
   # Prepare nix-index module with weekly updated database and comma integration
-  programs = let inherit (lib) mkDefault; in {
-    nix-index-database.comma.enable = mkDefault true; 
-    nix-index.enableBashIntegration = mkDefault false; 
-    nix-index.enableZshIntegration = mkDefault false; 
+  programs = let
+    inherit (lib) mkDefault;
+  in {
+    nix-index-database.comma.enable = mkDefault true;
+    nix-index.enableBashIntegration = mkDefault false;
+    nix-index.enableZshIntegration = mkDefault false;
     nix-index.enableFishIntegration = mkDefault false;
     command-not-found.enable = mkDefault false;
   };
-
 }
