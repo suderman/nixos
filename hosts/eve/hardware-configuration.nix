1c1e7dfe175a900f8eee24bd4c94ff70cd37e8a4
Jon Suderman <jon@suderman.net>
2025-07-23 13:45:52 -0600

    eve hardware

--- hosts/eve/hardware-configuration.nix
+++ hosts/eve/hardware-configuration.nix
@@ -16,26 +16,6 @@
   boot.kernelModules = ["kvm-intel"];
   boot.extraModulePackages = [];
 
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/820599c3-676b-4b0f-897d-0acc4d340530";
-    fsType = "btrfs";
-    options = ["subvol=root"];
-  };
-
-  fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/820599c3-676b-4b0f-897d-0acc4d340530";
-    fsType = "btrfs";
-  };
-
-  fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/C0B4-9840";
-    fsType = "vfat";
-  };
-
-  swapDevices = [
-    {device = "/dev/disk/by-uuid/83211ee8-9b8b-46c7-bf86-3e66b04b8215";}
-  ];
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
