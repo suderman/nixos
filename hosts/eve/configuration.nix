62b54765648409d43e4b03d143a5cbaf09ab4552
Jon Suderman <jon@suderman.net>
2025-07-23 13:35:37 -0600

    eve

--- hosts/eve/configuration.nix
+++ hosts/eve/configuration.nix
@@ -3,56 +3,62 @@
   flake,
   ...
 }: {
-  # Import all *.nix files in this directory
-  imports = flake.lib.ls ./.;
+  imports = [
+    ./hardware-configuration.nix
+    ./disk-configuration.nix
+    flake.nixosModules.common
+  ];
 
-  networking.domain = "work";
+  config = {
+    networking.domain = "work";
+    networking.firewall.allowPing = true;
 
-  # Use freshest kernel
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+    # Use freshest kernel
+    boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    # Use the systemd-boot EFI boot loader.
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  # Serve CA cert on http://10.2.0.2:1234
-  services.traefik.caPort = 1234;
+    # Serve CA cert on http://10.2.0.2:1234
+    services.traefik.caPort = 1234;
 
-  # # Backup media server
-  # services.jellyfin.enable = true;
-  #
-  # # Point /media to /backups/lux/media.* (latest version)
-  # systemd.services.media-symlink = {
-  #   description = "Update /media to point to latest lux backup";
-  #   after = ["multi-user.target"];
-  #   requires = ["multi-user.target"];
-  #   wantedBy = ["sysinit.target"];
-  #   serviceConfig = {
-  #     Type = "oneshot";
-  #     RemainAfterExit = "yes";
-  #   };
-  #   path = with pkgs; [coreutils];
-  #   script = ''
-  #     rm -f /media
-  #     ln -s "$(ls -td /backups/lux/media.* | head -n1)" /media
-  #   '';
-  # };
-  #
-  # # Run this script every day
-  # systemd.timers."media-symlink" = {
-  #   wantedBy = ["timers.target"];
-  #   partOf = ["media-symlink.service"];
-  #   timerConfig = {
-  #     OnCalendar = "daily";
-  #     Unit = "media-symlink.service";
-  #   };
-  # };
+    # # Backup media server
+    # services.jellyfin.enable = true;
+    #
+    # # Point /media to /backups/lux/media.* (latest version)
+    # systemd.services.media-symlink = {
+    #   description = "Update /media to point to latest lux backup";
+    #   after = ["multi-user.target"];
+    #   requires = ["multi-user.target"];
+    #   wantedBy = ["sysinit.target"];
+    #   serviceConfig = {
+    #     Type = "oneshot";
+    #     RemainAfterExit = "yes";
+    #   };
+    #   path = with pkgs; [coreutils];
+    #   script = ''
+    #     rm -f /media
+    #     ln -s "$(ls -td /backups/lux/media.* | head -n1)" /media
+    #   '';
+    # };
+    #
+    # # Run this script every day
+    # systemd.timers."media-symlink" = {
+    #   wantedBy = ["timers.target"];
+    #   partOf = ["media-symlink.service"];
+    #   timerConfig = {
+    #     OnCalendar = "daily";
+    #     Unit = "media-symlink.service";
+    #   };
+    # };
 
-  # # Snapshots & backup
-  # services.btrbk.enable = true;
-  #
-  # # Additional filesystems in motd
-  # programs.rust-motd.settings.filesystems = {
-  #   pool = "/mnt/pool";
-  # };
+    # # Snapshots & backup
+    # services.btrbk.enable = true;
+    #
+    # # Additional filesystems in motd
+    # programs.rust-motd.settings.filesystems = {
+    #   pool = "/mnt/pool";
+    # };
+  };
 }
