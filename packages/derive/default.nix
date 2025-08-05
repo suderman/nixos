7354cfb81b0925b3bf9fc23b976c9422df1a566f
Jon Suderman <jon@suderman.net>
2025-08-04 23:38:08 -0600

    refactor derive

--- packages/derive/default.nix
+++ packages/derive/default.nix
@@ -1,94 +1,27 @@
 {
   pkgs,
   perSystem,
-  flake,
   ...
-}: let
-  inherit (builtins) readFile;
-  inherit (pkgs) gnugrep openssh openssl age ssh-to-age;
-  python3 = pkgs.python3.withPackages (ps: [ps.cryptography]);
-in
-  perSystem.self.mkScript {
-    name = "derive";
-    path = [gnugrep openssl openssh python3 age ssh-to-age];
-    text =
-      # bash
-      ''
-        source ${flake.lib.bash}
+}:
+perSystem.self.mkScript {
+  name = "derive";
+  path = [
+    (pkgs.python3.withPackages (ps: [ps.cryptography]))
+    pkgs.age
+    pkgs.gnugrep
+    pkgs.openssh
+    pkgs.openssl
+    pkgs.ssh-to-age
+  ];
 
-        # If standard input is missing, change format to help
-        input="$(input)"
-        [[ -z "$input" ]] && format=help
+  # Paths to python scripts
+  env = {
+    path_to_cert_py = ./cert.py;
+    path_to_hex_py = ./hex.py;
+    path_to_key_py = ./key.py;
+    path_to_ssh_py = ./ssh.py;
+  };
 
-        # Convert based on format: age|hex|public|ssh
-        case "''${1-}" in
-
-          age | a)
-            ${readFile ./age.sh}
-            ;;
-
-          cert | c)
-            cakey="$(echo "$input" | $0 key)"
-            cacert="$(echo "$cakey" | python3 ${./cert.py})"
-            if [[ -z "''${@:2}" ]]; then
-              echo "$cacert"
-            else
-              common_name=''${2-}
-              key=$(echo "$input" | $0 key $common_name)
-              echo "$key" | python3 ${./cert.py} \
-                --name $common_name \
-                --cacert <(echo "$cacert") \
-                --cakey <(echo "$cakey")
-            fi
-            ;;
-
-          hex | h)
-            salt=''${2-} # optional salt, optional character length (default 64)
-            len=''${3:-64} && [[ "$len" =~ ^[0-9]+$ ]] && (( len >= 1 )) || len=""
-            if [[ -z "''${@:2}" ]]; then
-              echo "$input" | python3 ${./hex.py}
-            else
-              echo "$input" | python3 ${./hex.py} "$salt" | cut -c 1-$len
-            fi
-            ;;
-
-          key | k)
-            if [[ ! -z "$(echo "$input" | grep "BEGIN PRIVATE KEY")" ]]; then
-              echo "$input"
-            else
-              echo "$input" | $0 hex ''${2-} | python3 ${./key.py}
-            fi
-            ;;
-
-          public | p)
-            comment=''${2-} # optional ssh comment
-            ${readFile ./public.sh}
-            ;;
-
-          ssh | s)
-            passphrase=''${2-} # optional passphrase
-            if [[ -z "''${@:2}" ]]; then
-              echo "$input" | python3 ${./ssh.py}
-            else
-              key=$(mktemp) # write key to tmp file so passphrase can be added
-              echo "$input" | python3 ${./ssh.py} > $key
-              ssh-keygen -p -f $key -P "" -N "$passphrase" > /dev/null 2>&1
-              cat $key
-              shred -u $key # delete tmp key after sending to stdout
-            fi
-            ;;
-
-          help | *)
-            echo "Usage: echo 123 | derive FORMAT [ARGS]"
-            echo
-            echo "  age"
-            echo "  cert [NAME]"
-            echo "  hex [SALT] [LEN]"
-            echo "  key [NAME]"
-            echo "  public [COMMENT]"
-            echo "  ssh [PASSPHRASE]"
-            echo "  help"
-            ;;
-        esac
-      '';
-  }
+  # Bash script
+  text = builtins.readFile ./derive.sh;
+}
---
+++ packages/derive/derive.sh
@@ -0,0 +1,215 @@
+#! /usr/bin/env bash
+set -euo pipefail
+
+# ---------------------------------------------------------------------
+# MAIN
+# ---------------------------------------------------------------------
+main() {
+
+  local cmd="${1-}"
+  shift
+
+  # If standard input is missing, change cmd to help
+  local input
+  input="$([ -t 0 ] || cat)"
+  [[ -z "$input" ]] && cmd="help"
+
+  case "$cmd" in
+  age | a)
+    derive_age "$@" <<<"$input"
+    ;;
+  cert | c)
+    derive_cert "$@" <<<"$input"
+    ;;
+  hex | h)
+    derive_hex "$@" <<<"$input"
+    ;;
+  key | k)
+    derive_key "$@" <<<"$input"
+    ;;
+  public | p)
+    derive_public "$@" <<<"$input"
+    ;;
+  ssh | s)
+    derive_ssh "$@" <<<"$input"
+    ;;
+  help | *)
+    help
+    ;;
+  esac
+
+}
+
+# ---------------------------------------------------------------------
+# HELP
+# ---------------------------------------------------------------------
+help() {
+  cat <<EOF
+Usage: derive FORMAT [ARGS] <<<123
+
+  age
+  cert [NAME]
+  hex [SALT] [LEN]
+  key [NAME]
+  public [COMMENT]
+  ssh [PASSPHRASE]
+  help
+EOF
+}
+
+# ---------------------------------------------------------------------
+# DERIVE_AGE
+# ---------------------------------------------------------------------
+derive_age() {
+
+  # Exit if standard input is missing
+  local input
+  input="$([ -t 0 ] || cat)"
+  [[ -z "$input" ]] && exit 0
+
+  # Use derive ssh (this package) to generate ssh key from input
+  local ssh
+  ssh="$(derive_ssh <<<"$input")"
+  [[ -z "$ssh" ]] && exit 0
+
+  # Use https://github.com/Mic92/ssh-to-age to generate age from ssh key
+  local age
+  age="$(ssh-to-age -private-key <<<"$ssh")"
+  [[ -z "$age" ]] && exit 0
+
+  # Use derive public (this package) to generate formatted identity and output
+  echo "# imported from: $(derive_public <<<"$ssh")"
+  echo "# public key: $(derive_public <<<"$age")"
+  echo "$age"
+
+}
+
+# ---------------------------------------------------------------------
+# DERIVE_CERT
+# ---------------------------------------------------------------------
+derive_cert() {
+
+  # Exit if standard input is missing
+  local input
+  input="$([ -t 0 ] || cat)"
+  [[ -z "$input" ]] && exit 0
+
+  local cakey
+  cakey="$(derive_key <<<"$input")"
+
+  local cacert
+  cacert="$(python3 "${path_to_cert_py-}" <<<"$cakey")"
+
+  local common_name
+  common_name="${2-}"
+
+  if [[ -z "$common_name" ]]; then
+    echo "$cacert"
+  else
+    python3 "${path_to_cert_py-}" \
+      --name "$common_name" \
+      --cacert <(echo "$cacert") \
+      --cakey <(echo "$cakey") \
+      <<<"$(derive_key "$common_name" <<<"$input")"
+  fi
+
+}
+
+# ---------------------------------------------------------------------
+# DERIVE_HEX
+# ---------------------------------------------------------------------
+derive_hex() {
+
+  # Exit if standard input is missing
+  local input
+  input="$([ -t 0 ] || cat)"
+  [[ -z "$input" ]] && exit 0
+
+  local salt="${1-}" # optional salt, optional character length (default 64)
+  local len="${2:-64}" && [[ "$len" =~ ^[0-9]+$ ]] && ((len >= 1)) || len=""
+  if [[ -z "$salt" ]]; then
+    python3 "${path_to_hex_py-}" <<<"$input"
+  else
+    python3 "${path_to_hex_py-}" "$salt" <<<"$input" | cut -c 1-"$len"
+  fi
+
+}
+
+# ---------------------------------------------------------------------
+# DERIVE_KEY
+# ---------------------------------------------------------------------
+derive_key() {
+
+  # Exit if standard input is missing
+  local input
+  input="$([ -t 0 ] || cat)"
+  [[ -z "$input" ]] && exit 0
+
+  if grep -q "BEGIN PRIVATE KEY" <<<"$input"; then
+    echo "$input"
+  else
+    local name="${1-}" # optional key name
+    derive_hex "$name" <<<"$input" | python3 "${path_to_key_py-}"
+  fi
+
+}
+
+# ---------------------------------------------------------------------
+# DERIVE_PUBLIC
+# ---------------------------------------------------------------------
+derive_public() {
+
+  # Exit if standard input is missing
+  local input
+  input="$([ -t 0 ] || cat)"
+  [[ -z "$input" ]] && exit 0
+
+  # If age identity detected, extract recipient from secret and output
+  if grep -q "AGE-SECRET-KEY" <<<"$input"; then
+    age-keygen -y <<<"$input"
+
+  # If ssh ed25519 detected, extract public key from secret and output
+  elif grep -q "OPENSSH PRIVATE KEY" <<<"$input"; then
+    local comment="${1-}" # optional ssh comment
+    echo "$(ssh-keygen -y -f <(echo "$input") | cut -d ' ' -f 1,2) $comment"
+
+  # If ed25519 key detected, extract private key from secret and output
+  elif grep -q "BEGIN PRIVATE KEY" <<<"$input"; then
+    openssl ec -in <(echo "$input") -pubout 2>/dev/null
+
+  # If certificate detected, extract private key from secret and output
+  elif grep "BEGIN CERTIFICATE" <<<"$input"; then
+    openssl x509 -in <(echo "$input") -pubkey -noout 2>/dev/null
+
+  # Fallback on echoing the input to output
+  else
+    echo "$input"
+  fi
+
+}
+
+# ---------------------------------------------------------------------
+# DERIVE_SSH
+# ---------------------------------------------------------------------
+derive_ssh() {
+
+  # Exit if standard input is missing
+  local input
+  input="$([ -t 0 ] || cat)"
+  [[ -z "$input" ]] && exit 0
+
+  local passphrase="${1-}" # optional passphrase
+  if [[ -z "$passphrase" ]]; then
+    python3 "${path_to_ssh_py-}" <<<"$input"
+  else
+    local key
+    key="$(mktemp)" # write key to tmp file so passphrase can be added
+    python3 "${path_to_ssh_py-}" <<<"$input" >"$key"
+    ssh-keygen -p -f "$key" -P "" -N "$passphrase" >/dev/null 2>&1
+    cat "$key"
+    shred -u "$key" # delete tmp key after sending to stdout
+  fi
+
+}
+
+main "${@-}"
