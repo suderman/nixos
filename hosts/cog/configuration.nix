1f7f809247b6c7c77052979d0c2367ab621bf298
Jon Suderman <jon@suderman.net>
2025-09-02 16:19:59 -0600

    cog hardware

--- hosts/cog/configuration.nix
+++ hosts/cog/configuration.nix
@@ -1,7 +1,62 @@
-{flake, ...}: {
+{
+  config,
+  pkgs,
+  inputs,
+  flake,
+  ...
+}: {
   imports = [
+    ./hardware-configuration.nix
+    ./disk-configuration.nix
+    inputs.hardware.nixosModules.common-gpu-nvidia-nonprime
     flake.nixosModules.common
     flake.nixosModules.extra
+    flake.nixosModules.hyprland
   ];
+
   networking.domain = "tail";
+
+  # Use freshest kernel
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  # Good graphics
+  hardware.graphics.extraPackages = [
+    pkgs.mesa.drivers
+    pkgs.vaapiVdpau
+  ];
+
+  # Snapshots and backups
+  services.btrbk.volumes = {
+    "/mnt/main" = ["ssh://fit/mnt/pool/backups/${config.networking.hostName}"];
+  };
+
+  # Sound & Bluetooth
+  services.pipewire.enable = true;
+  security.rtkit.enable = true;
+  hardware.bluetooth.enable = true;
+
+  # Laptop-specific
+  services.fwupd.enable = true; # sudo fwupdmgr update
+  services.thermald.enable = true; # Lower fan noise
+
+  # Power management
+  services.power-profiles-daemon.enable = false;
+  services.tlp.enable = true;
+  services.tlp.settings.SATA_LINKPWR_ON_BAT = "max_performance";
+
+  # Allow powerkey to be intercepted, but still poweroff for longpress
+  services.logind = {
+    powerKey = "ignore";
+    powerKeyLongPress = "poweroff";
+    lidSwitch = "suspend";
+    lidSwitchExternalPower = "suspend";
+    lidSwitchDocked = "ignore";
+  };
+
+  # Keyboard control
+  services.keyd = {
+    enable = true;
+    quirks = true;
+    keyboard = config.services.keyd.internalKeyboards.framework;
+  };
 }
---
+++ hosts/cog/disk-configuration.nix
@@ -0,0 +1,102 @@
+{disks ? [], ...}: let
+  # Named disk devices
+  dev = {
+    ssd1 = "nvme-WD_BLACK_SN7100_2TB_2452HQ400732";
+  };
+
+  # Create named disk attr if name found in disks list OR if disks is empty list
+  disk = name: partitions:
+    if disks == [] || (builtins.hasAttr name dev && builtins.elem name disks)
+    then {
+      "${name}" = {
+        type = "disk";
+        device = "/dev/disk/by-id/${builtins.getAttr name dev}";
+        content = {
+          type = "gpt";
+          inherit partitions;
+        };
+      };
+    }
+    else {};
+
+  # Default btrfs mount options with mountpoint
+  mount = mountpoint: {
+    inherit mountpoint;
+    mountOptions = [
+      "compress=zstd" # enable zstd compression
+      "space_cache=v2" # track available free space on filesystem
+      "discard=async" # free up deleted space in the background
+      "noatime" # disables access time updates on files
+    ];
+  };
+
+  # Extended mount options to support automount
+  automount = mountpoint: {
+    inherit mountpoint;
+    mountOptions =
+      (mount null).mountOptions
+      ++ [
+        "noauto" # do not mount on boot
+        "nofail" # continue boot even if disk is missing
+        "x-systemd.automount" # create automount unit to mount when accessed
+        "x-systemd.device-timeout=1ms" # assume device is already plugged in and do not wait
+        "x-systemd.idle-timeout=5m" # unmount after 5 min of inactivity
+      ];
+  };
+in {
+  disko.devices.disk =
+    # main disk
+    # disko disk-configuration.nix -m destroy,format,mount --arg disks '["ssd1"]'
+    disk "ssd1" {
+      # bios boot
+      grub = {
+        size = "1M";
+        type = "EF02";
+        priority = 1;
+      };
+
+      # uefi boot
+      boot = {
+        size = "4G";
+        type = "EF00";
+        priority = 2;
+        content = {
+          type = "filesystem";
+          format = "vfat";
+          mountpoint = "/boot";
+          mountOptions = ["umask=0077"];
+        };
+      };
+
+      # adjust size to match ram
+      swap = {
+        size = "32G";
+        priority = 3;
+        content = {
+          type = "swap";
+          discardPolicy = "both";
+          resumeDevice = true; # support hibernation
+        };
+      };
+
+      # main partition
+      part = {
+        size = "100%";
+        priority = 4;
+        content =
+          mount "/mnt/main"
+          // {
+            type = "btrfs";
+            extraArgs = ["-fL main"];
+            subvolumes = {
+              root = mount "/";
+              nix = mount "/nix";
+              scratch = {};
+              storage = {};
+              snapshots = {};
+              backups = {};
+            };
+          };
+      };
+    };
+}
