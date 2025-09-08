ff6105fec6d79c27c3d74c7326dd0d9265795c0a
Jon Suderman <jon@suderman.net>
2025-09-08 11:19:52 -0600

    disable hyprland expo

--- modules/home/hyprland/settings/hyprexpo.nix
+++ modules/home/hyprland/settings/hyprexpo.nix
@@ -9,30 +9,30 @@
 in {
   wayland.windowManager.hyprland = {
     # plugins = mkIf cfg.enablePlugins [pkgs.hyprlandPlugins.hyprexpo];
-    plugins = mkIf cfg.enablePlugins [perSystem.nixpkgs-unstable.hyprlandPlugins.hyprexpo];
+    # plugins = mkIf cfg.enablePlugins [perSystem.nixpkgs-unstable.hyprlandPlugins.hyprexpo];
 
-    settings = {
-      "plugin:hyprexpo" = {
-        columns = 3;
-        gap_size = 5;
-        bg_col = "rgb(111111)";
-        workspace_method = "first 1";
-
-        enable_gesture = true; # laptop touchpad
-        gesture_fingers = 3; # 3 or 4
-        gesture_distance = 300; # how far is the "max"
-        gesture_positive = true; # positive = swipe down. Negative = swipe up.
-      };
-
-      bind = mkIf cfg.enablePlugins [
-        "super, 0, hyprexpo:expo, toggle"
-      ];
-
-      bindsn = mkIf cfg.enablePlugins [
-        # Toggle expo with super+semicolon+apostrophe ([;'] same-time)
-        "super_l, semicolon&apostrophe, hyprexpo:expo, toggle"
-        "super_r, semicolon&apostrophe, hyprexpo:expo, toggle"
-      ];
-    };
+    # settings = {
+    #   "plugin:hyprexpo" = {
+    #     columns = 3;
+    #     gap_size = 5;
+    #     bg_col = "rgb(111111)";
+    #     workspace_method = "first 1";
+    #
+    #     enable_gesture = true; # laptop touchpad
+    #     gesture_fingers = 3; # 3 or 4
+    #     gesture_distance = 300; # how far is the "max"
+    #     gesture_positive = true; # positive = swipe down. Negative = swipe up.
+    #   };
+    #
+    #   bind = mkIf cfg.enablePlugins [
+    #     "super, 0, hyprexpo:expo, toggle"
+    #   ];
+    #
+    #   bindsn = mkIf cfg.enablePlugins [
+    #     # Toggle expo with super+semicolon+apostrophe ([;'] same-time)
+    #     "super_l, semicolon&apostrophe, hyprexpo:expo, toggle"
+    #     "super_r, semicolon&apostrophe, hyprexpo:expo, toggle"
+    #   ];
+    # };
   };
 }
