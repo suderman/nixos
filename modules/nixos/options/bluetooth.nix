dabfd76f30b955e5aada905bf73f004cae2fb409
Jon Suderman <jon@suderman.net>
2025-09-13 21:57:43 -0600

    bluetooth

---
+++ modules/nixos/options/bluetooth.nix
@@ -0,0 +1,34 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: {
+  config = lib.mkIf config.hardware.bluetooth.enable {
+    # Bluetuith client, and throw in bluetui as well
+    environment.systemPackages = [
+      pkgs.bluez
+      pkgs.bluetuith
+      pkgs.bluetui
+    ];
+
+    # Root config for bluetuith (vim bindings)
+    system.activationScripts.bluetuith = ''
+      mkdir -p /root/.bluetuith
+      printf '${builtins.toJSON {
+        theme = {};
+        receive-dir = "";
+        keybindings = {
+          NavigateDown = "j";
+          NavigateUp = "k";
+          Menu = "l";
+          Close = "h";
+          Quit = "q";
+        };
+      }}' >/root/.bluetuith/bluetuith.conf
+    '';
+
+    # Persist bluetooth parings between reboots
+    persist.storage.directories = ["/var/lib/bluetooth"];
+  };
+}
