6dd4b97a2882c05101ab747fe551502b087115d8
Jon Suderman <jon@suderman.net>
2025-09-17 14:24:42 -0600

    tautulli

---
+++ modules/nixos/default/options/tautulli.nix
@@ -0,0 +1,30 @@
+# services.tautulli.enable = true;
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.services.tautulli;
+  inherit (lib) mkIf mkOption types;
+in {
+  options.services.tautulli = {
+    name = mkOption {
+      type = types.str;
+      default = "tautulli";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    services.tautulli = {
+      user = "plexpy";
+      group = "nogroup";
+      port = cfg.port;
+      openFirewall = true;
+    };
+
+    services.traefik = {
+      enable = true;
+      proxy.${cfg.name} = "http://127.0.0.1:${toString cfg.port}"; # 8181
+    };
+  };
+}
