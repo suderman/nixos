5e89153d9353161ed2da5d4821d18a083bf39415
Jon Suderman <jon@suderman.net>
2025-09-25 11:51:55 -0600

    remove redundant rules

--- modules/nixos/default/options/dolphin-emu.nix
+++ modules/nixos/default/options/dolphin-emu.nix
@@ -4,24 +4,9 @@
   lib,
   flake,
   ...
-}: let
-  inherit (lib) mkIf;
-  # If any home-manager dolphin-emu is enabled for any user, set this to true
-  enable = flake.lib.anyUser config (user: user.programs.dolphin-emu.enable);
-in {
-  config = mkIf enable {
+}: {
+  # https://github.com/dolphin-emu/dolphin/blob/master/Data/51-usb-device.rules
+  config = lib.mkIf (flake.lib.anyUser config (u: u.programs.dolphin-emu.enable)) {
     services.udev.packages = [pkgs.dolphin-emu];
-
-    # https://github.com/dolphin-emu/dolphin/blob/master/Data/51-usb-device.rules
-    services.udev.extraRules = lib.mkAfter ''
-
-      # GameCube Controller Adapter
-      SUBSYSTEM=="usb", ENV{DEVTYPE}=="usb_device", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0337", TAG+="uaccess"
-
-      # Wiimotes or DolphinBar
-      SUBSYSTEM=="hidraw*", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0306", TAG+="uaccess"
-      SUBSYSTEM=="hidraw*", ATTRS{idVendor}=="057e", ATTRS{idProduct}=="0330", TAG+="uaccess"
-
-    '';
   };
 }
