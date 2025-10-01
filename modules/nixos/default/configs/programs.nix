162fa9cd1cfcfee9f2e2e48f38a0cbe8b552a569
Jon Suderman <jon@suderman.net>
2025-10-01 11:23:13 -0600

    shared programs for default, desktop

--- modules/nixos/default/configs/programs.nix
+++ modules/nixos/default/configs/programs.nix
@@ -4,35 +4,37 @@
   ...
 }: {
   # List packages installed in system profile
-  environment.systemPackages = [
-    pkgs.arp-scan
-    pkgs.bash-completion
-    pkgs.btop
-    pkgs.cachix # binary cache
-    pkgs.curl
-    pkgs.dig
-    pkgs.git
-    pkgs.gnumake
-    pkgs.home-manager
-    pkgs.htop
-    pkgs.inetutils
-    pkgs.jq
-    pkgs.lsof
-    pkgs.mtr
-    pkgs.nix-bash-completions
-    pkgs.nix-zsh-completions
-    pkgs.nmap
-    pkgs.pciutils
-    pkgs.rsync
-    pkgs.sysstat
-    pkgs.tmux
-    pkgs.unzip
-    pkgs.usbutils
-    pkgs.zip
-    pkgs.zsh-completions
+  environment.systemPackages = with pkgs; [
+    arp-scan # arp-fingerprint arp-scan get-iab get-oui
+    btop
+    cachix
+    curl # curl curl-config wcurl
+    dig # arpaname ddns-confgen delv dig dnssec-cds dnssec-dsfromkey dnssec-importkey dnssec-keyfromlabel dnssec-keygen dnssec-ksr dnssec-revoke dnssec-settime dnssec-signzone dnssec-verify host mdig named named-checkconf named-checkzone named-compilezone named-journalprint named-rrchecker nsec3hash nslookup nsupdate rndc rndc-confgen tsig-keygen
+    gnumake # make
+    gnutar # tar
+    home-manager
+    htop
+    inetutils # dnsdomainname ftp hostname ifconfig logger ping ping6 rcp rexec rlogin rsh talk telnet tftp traceroute whois
+    jq
+    libarchive # bsdcat bsdcpio bsdtar bsdunzip
+    lsof
+    mtr # mtr mtr-packet
+    nmap # ncat nmap nping
+    p7zip # 7z 7za 7zr
+    pciutils # lspci pcilmr setpci
+    rsync # rsync rsync-ssl
+    sysstat # cifsiostat iostat mpstat pidstat sadf sar tapestat
+    unzip # funzip unzip unzipsfx zipgrep zipinfo
+    usbutils # lsusb lsusb.py usb-devices usbhid-dump usbreset
+    zip # zip zipcloak zipnote zipsplit
   ];
 
   # Default enable these common modules for all hosts
-  programs.mosh.enable = lib.mkDefault true;
-  programs.rust-motd.enable = lib.mkDefault true;
+  programs = {
+    git.enable = lib.mkDefault true;
+    mosh.enable = lib.mkDefault true;
+    rust-motd.enable = lib.mkDefault true;
+    tmux.enable = lib.mkDefault true;
+    zsh.enable = lib.mkDefault true;
+  };
 }
--- modules/nixos/desktops/default/programs.nix
+++ modules/nixos/desktops/default/programs.nix
@@ -1,4 +1,13 @@
-{...}: {
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    desktop-file-utils # desktop-file-edit desktop-file-install desktop-file-validate update-desktop-database
+    hicolor-icon-theme # fallback icon theme
+    shared-mime-info # update-mime-database
+    wl-clipboard # wl-copy wl-paste
+    xdg-user-dirs # xdg-user-dir xdg-user-dirs-update
+    xdg-utils # xdg-desktop-icon xdg-desktop-menu xdg-email xdg-icon-resource xdg-mime xdg-open xdg-screensaver xdg-settings xdg-terminal
+  ];
+
   # AirDrop alternative
   programs.localsend.enable = true;
 }
