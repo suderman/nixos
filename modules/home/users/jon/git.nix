f5cbb2732b05086d86e565a1bf1ca3ff52b37075
Jon Suderman <jon@suderman.net>
2025-10-06 17:55:01 -0600

    git-credentials

--- modules/home/users/jon/git.nix
+++ modules/home/users/jon/git.nix
@@ -1,4 +1,8 @@
-{config, ...}: {
+{
+  config,
+  lib,
+  ...
+}: {
   programs.git = {
     enable = true;
     extraConfig.user = {
@@ -8,11 +12,7 @@
   };
 
   age.secrets.git-credentials.rekeyFile = ./git-credentials.age;
-  tmpfiles.files = [
-    {
-      target = ".git-credentials";
-      source = config.age.secrets.git-credentials.path;
-      mode = 600;
-    }
-  ];
+  home.activation.git-credentials = lib.hm.dag.entryAfter ["linkGeneration"] ''
+    cat ${config.age.secrets.git-credentials.path} >${config.home.homeDirectory}/.git-credentials
+  '';
 }
