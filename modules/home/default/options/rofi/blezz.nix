c2567e7692c310a9879fe96b611c9209c0276b2e
Jon Suderman <jon@suderman.net>
2025-10-17 12:13:19 -0600

    blezz

--- modules/home/default/options/rofi/blezz.nix
+++ modules/home/default/options/rofi/blezz.nix
@@ -29,20 +29,30 @@ in {
 
     xdg.configFile."rofi/blezz".text = ''
       Main:
-      act(r, run, rofi -show run)
-      dir(a, Audio, audio-headphones)
       dir(p, Programs, window-new-symbolic)
+      dir(m, Media, audio-headphones)
+      dir(t, Toggle, applications-system)
+      act(r, run, rofi -show run)
 
-      Audio:
+      Programs:
+      act(k, Kitty, kitty)
+      act(c, Chromium, chromium-browser)
+      act(f, Firefox, firefox)
+
+      Media:
       actReload(a, Volume Down, volumectl -pb down, audio-volume-low)
       actReload(s, Volume Up, volumectl -pbu up, audio-volume-high)
       actReload(d, Mute, volumectl -a toggle-mute, audio-volume-muted)
       actReload(c, Mute Microphone, volumectl -am toggle-mute, audio-input-microphone)
       actReload(p, Play/Pause, playerctl play-pause, media-playback-start)
+      actReload(f, Forward Play, playerctl next, media-skip-forward)
+      actReload(r, Reverse Play, playerctl previous, media-skip-backward)
+      actReload(z, Brightness Down, lightctl down, video-display)
+      actReload(x, Brightness Up, lightctl up, video-display)
+      act(m, Mixer, kitty ncpamixer, preferences-desktop-sound)
 
-      Programs:
-      act(f, Firefox, firefox)
-      act(c, Chromium, chromium-browser)
+      Toggle:
+      actReload(t, Title Bars, hypr-toggletitlebars, preferences-desktop)
     '';
 
     # Use a real file for blezz to ease real-time tinkering
