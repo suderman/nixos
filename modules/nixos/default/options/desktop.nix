72005c41545d0e85442132e9a64bdc46a36fd945
Jon Suderman <jon@suderman.net>
2025-10-18 08:38:43 -0600

    fix typo

--- modules/nixos/default/options/desktop.nix
+++ modules/nixos/default/options/desktop.nix
@@ -1,4 +1,4 @@
-# This option is set true by homeModules.desktops.default
+# This option is set true by nixosModules.desktops.default
 # desktop.enable = true;
 #
 # Can be used elsewhere to set config exclusively on desktop
