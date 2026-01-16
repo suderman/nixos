48980d593470814e4fa13465f5934c79583fa897
Jon Suderman <jon@suderman.net>
2026-01-16 13:51:27 -0700

    argh

--- hosts/pow/users/jon.nix
+++ hosts/pow/users/jon.nix
@@ -1,15 +1,15 @@
 {flake, ...}: {
   imports = [
     flake.homeModules.default
-    flake.homeModules.desktop.hyprland
+    # flake.homeModules.desktop.hyprland
     flake.homeModules.users.jon
   ];
 
-  # Hyprland on AMD desktop
-  wayland.windowManager.hyprland = {
-    settings.exec-once = ["freetube" "zwift"];
-    enablePlugins = true; # set false if plugins barf errors
-  };
-
-  programs.zwift.enable = true; # fitness
+  # # Hyprland on AMD desktop
+  # wayland.windowManager.hyprland = {
+  #   settings.exec-once = ["freetube" "zwift"];
+  #   enablePlugins = true; # set false if plugins barf errors
+  # };
+  #
+  # programs.zwift.enable = true; # fitness
 }
