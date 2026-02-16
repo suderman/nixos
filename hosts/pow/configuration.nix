e26f792d315e4046ef70bfad52dd27d2a53a3ecf
Jon Suderman <jon@suderman.net>
2026-02-16 13:00:02 -0700

    pow

--- hosts/pow/configuration.nix
+++ hosts/pow/configuration.nix
@@ -29,6 +29,7 @@
     "broadcom-sta-6.30.223.271-59-6.12.63"
     "broadcom-sta-6.30.223.271-59-6.12.65"
     "broadcom-sta-6.30.223.271-59-6.12.67"
+    "broadcom-sta-6.30.223.271-59-6.12.69"
   ];
 
   # Bigger banana
