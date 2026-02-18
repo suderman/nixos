f8d73c7cff991e4cd18c3d35ca12d8a8968e0ad3
Jon Suderman <jon@suderman.net>
2026-02-18 10:16:37 -0700

    persist npm directory

--- modules/home/default/options/languages/javascript.nix
+++ modules/home/default/options/languages/javascript.nix
@@ -5,19 +5,23 @@
   ...
 }: let
   cfg = config.programs.javascript;
+  npmDir = ".local/share/npm";
   pnpmDir = ".local/share/pnpm";
   bunDir = ".local/share/bun";
 in {
   options.programs.javascript.enable = lib.mkEnableOption "javascript";
   config = lib.mkIf cfg.enable {
-    persist.scratch.directories = [pnpmDir bunDir];
+    persist.scratch.directories = [npmDir pnpmDir bunDir];
     home.sessionVariables = rec {
+      NPM_CONFIG_PREFIX = "${config.home.homeDirectory}/${npmDir}";
+      NPM_CONFIG_CACHE = "${config.home.homeDirectory}/${npmDir}/cache";
       PNPM_HOME = "${config.home.homeDirectory}/${pnpmDir}";
       PNPM_STORE_DIR = "${PNPM_HOME}/store";
       BUN_INSTALL = "${config.home.homeDirectory}/${bunDir}";
       BUN_INSTALL_CACHE_DIR = "${BUN_INSTALL}/install/cache";
     };
     home.sessionPath = [
+      "${config.home.homeDirectory}/${npmDir}/bin"
       "${config.home.homeDirectory}/${pnpmDir}"
       "${config.home.homeDirectory}/${bunDir}/bin"
     ];
