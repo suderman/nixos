0d572144c57edeacb2b8772b038711b6cc989018
Jon Suderman <jon@suderman.net>
2026-04-15 20:34:29 -0600

    format: apply nixfmt to lib/genAttrs.nix and lib/networking.nix

--- lib/genAttrs.nix
+++ lib/genAttrs.nix
@@ -1,22 +1,32 @@
 # Extend nixpkgs' genAttrs to first convert provided paths or attrs to a list
-{ lib, flake, ... }: x: fn: let
-
+{
+  lib,
+  flake,
+  ...
+}: x: fn: let
   # Ensure string and strip .nix suffix from any entries
   fromList = list: map (name: lib.removeSuffix ".nix" (toString name)) list;
 
   # List of directory and filenames in given path
-  fromPath = path: fromList ( flake.lib.ls { 
-    inherit path; asPath = false; dirsExcept = []; 
-  });
+  fromPath = path:
+    fromList (flake.lib.ls {
+      inherit path;
+      asPath = false;
+      dirsExcept = [];
+    });
 
   # List of attribute names in given attr set
   fromAttrs = attrs: fromList (builtins.attrNames attrs);
 
   inherit (builtins) isAttrs isPath isList;
-  list = if (isPath x) then (fromPath x)
-    else if (isList x) then (fromList x)
-    else if (isAttrs x) then (fromAttrs x)
+  list =
+    if (isPath x)
+    then (fromPath x)
+    else if (isList x)
+    then (fromList x)
+    else if (isAttrs x)
+    then (fromAttrs x)
     else [];
-
-# Pass along modified list and provided function to nixpkgs's genAttrs
-in lib.genAttrs list fn
+  # Pass along modified list and provided function to nixpkgs's genAttrs
+in
+  lib.genAttrs list fn
--- lib/networking.nix
+++ lib/networking.nix
@@ -1,26 +1,31 @@
-# Attribute set describing my domains and IP addresses  
-{ lib, flake, ... }: let
-
+# Attribute set describing my domains and IP addresses
+{
+  lib,
+  flake,
+  ...
+}: let
   inherit (flake.lib) genAttrs;
-  inherit (lib) foldl filterAttrs mapAttrsToList mapAttrs' nameValuePair; 
+  inherit (lib) foldl filterAttrs mapAttrsToList mapAttrs' nameValuePair;
 
   # Centralized hierarchy of IP addresses
   zones = genAttrs ../zones (zone: import ../zones/${zone});
 
   # Determine primary IP address for each host from configuration domain
-  addresses = filterAttrs (n: v: v != "") ( genAttrs flake.nixosConfigurations ( hostName: let
+  addresses = filterAttrs (n: v: v != "") (genAttrs flake.nixosConfigurations (hostName: let
     inherit (flake.nixosConfigurations."${hostName}".config.networking) domain;
-    ip = if isNull domain then "" else (zones.${domain}.${hostName} or "");
-  in ip ) );
+    ip =
+      if isNull domain
+      then ""
+      else (zones.${domain}.${hostName} or "");
+  in
+    ip));
 
   # Flatten the tree into a "hostName.domain = address" set
-  flatten = tree: foldl (a: b: a // b) {}  ( 
-    mapAttrsToList (domain: hostNames: 
-    (mapAttrs' (hostName: ip: nameValuePair ("${hostName}.${domain}") ip) hostNames)) tree
-  );
-
+  flatten = tree:
+    foldl (a: b: a // b) {} (
+      mapAttrsToList (domain: hostNames: (mapAttrs' (hostName: ip: nameValuePair "${hostName}.${domain}" ip) hostNames)) tree
+    );
 in {
-
   # Self-signed CA certificate, domain name used for public services
   inherit (import ../zones) ca domainName;
 
@@ -29,6 +34,4 @@ in {
 
   # Internal DNS records
   records = (flatten zones) // addresses;
-
 }
-
