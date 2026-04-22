e54e3954368999bd4c7521098cd867db95963bec
Jon Suderman <jon@suderman.net>
2026-04-22 17:05:27 -0600

    update flake inputs and clean up firefox addon config

--- modules/home/desktop/default/options/firefox/addons.nix
+++ modules/home/desktop/default/options/firefox/addons.nix
@@ -55,10 +55,8 @@ in {
     "easy-container-shortcuts" = buildFirefoxXpiAddon {
       pname = "easy-container-shortcuts";
       version = "1.6.0";
-      # addonId = "{e52552b9-5280-4698-b818-257818153f8f}";
       addonId = "easy-container-shortcuts@extraAddons";
       url = "https://addons.mozilla.org/firefox/downloads/file/4068015/easy_container_shortcuts-1.6.0.xpi";
-      # sha256 = "sha256-B5MwcMObKuihMTyCGwd6QgX/RrYS5THfZbUosMMH9gc=";
       sha256 = "01zn0z1v0a5mcpgk3r8jnr3gy1a2g83ip0iw66hyhalvqdq314q7";
       meta = with lib; {
         description = "Easy, opinionated, keyboard shortcuts for Firefox 57+ containers.";
