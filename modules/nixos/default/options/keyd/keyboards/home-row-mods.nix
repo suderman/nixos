d61d256c2a96d1e03c89c1f2c3cd943b3a5ff362
Jon Suderman <jon@suderman.net>
2026-07-06 16:32:24 -0600

    feat(keyd): retune home-row mods and move media to comma
    Rotate home-row modifier positions:
      meta  = f j
      alt   = d k
      ctrl  = c m
      shift = s l
    
    The previous a/; super mapping is commented out along with the old
    s/l, d/k, f/j bindings. Move media from m to comma, since m is now ctrl.

--- modules/nixos/default/options/keyd/keyboards/home-row-mods.nix
+++ modules/nixos/default/options/keyd/keyboards/home-row-mods.nix
@@ -3,27 +3,43 @@
 #
 # [⌘a][⌥s][⌃d][⇧f] _ _ [⇧f][⌃d][⌥s][⌘;]
 {
-  # [⌘] super is [a][;]
-  a = "lettermod(meta, a, 200, 220)";
-  semicolon = "lettermod(meta, ;, 200, 220)";
+  # # [⌘] super is [a][;]
+  # a = "lettermod(meta, a, 200, 220)";
+  # semicolon = "lettermod(meta, ;, 200, 220)";
+  #
+  # # [⌥] alt is [s][l]
+  # s = "lettermod(alt, s, 200, 220)";
+  # l = "lettermod(alt, l, 200, 220)";
+  #
+  # # [⌃] ctrl is [d][k]
+  # d = "lettermod(control, d, 200, 220)";
+  # k = "lettermod(control, k, 200, 220)";
+  #
+  # # [⇧] shift is [f][j]
+  # f = "lettermod(shift, f, 200, 220)";
+  # j = "lettermod(shift, j, 200, 220)";
 
-  # [⌥] alt is [s][l]
-  s = "lettermod(alt, s, 200, 220)";
-  l = "lettermod(alt, l, 200, 220)";
+  # [⌘] super is [f][j]
+  f = "lettermod(meta, f, 200, 220)";
+  j = "lettermod(meta, j, 200, 220)";
 
-  # [⌃] ctrl is [d][k]
-  d = "lettermod(control, d, 200, 220)";
-  k = "lettermod(control, k, 200, 220)";
+  # [⌥] alt is [d][k]
+  d = "lettermod(alt, d, 200, 220)";
+  k = "lettermod(alt, k, 200, 220)";
 
-  # [⇧] shift is [f][j]
-  f = "lettermod(shift, f, 200, 220)";
-  j = "lettermod(shift, j, 200, 220)";
+  # [⌃] ctrl is [c][m]
+  c = "lettermod(control, c, 200, 220)";
+  m = "lettermod(control, m, 200, 220)";
+
+  # [⇧] shift is [s][l]
+  s = "lettermod(shift, s, 200, 220)";
+  l = "lettermod(shift, l, 200, 220)";
 
   # [✥] nav is [space]
   space = "lettermod(nav, space, 200, 220)";
 
-  # [♫] media is [m]
-  m = "lettermod(media, m, 200, 220)";
+  # [♫] media is [,]
+  comma = "lettermod(media, comma, 200, 220)";
 
   # [Fn] function is [r][u]
   u = "lettermod(fn, u, 200, 220)";
