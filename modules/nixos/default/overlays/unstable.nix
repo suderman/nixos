3595c53e56fcf83ca0b23a55a3f3bc8d5431602e
Jon Suderman <jon@suderman.net>
2026-07-10 17:34:19 -0600

    fix: repair kit after flake update

--- modules/nixos/default/overlays/unstable.nix
+++ modules/nixos/default/overlays/unstable.nix
@@ -4,6 +4,19 @@
       unstable = import flake.inputs.nixpkgs-unstable {
         system = prev.stdenv.hostPlatform.system;
         config = prev.config;
+        overlays = [
+          (_final: unstablePrev: {
+            pythonPackagesExtensions =
+              (unstablePrev.pythonPackagesExtensions or [])
+              ++ [
+                (_pyFinal: pyPrev: {
+                  click-threading = pyPrev.click-threading.overridePythonAttrs (_old: {
+                    enabledTestPaths = ["tests"];
+                  });
+                })
+              ];
+          })
+        ];
       };
     })
   ];
